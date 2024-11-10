c9114f71422892be43e514cacecf30f32b8ead57
e.kolokolov <lelik.bot+vsv@gmail.com>
2024-11-10 18:49:47 +0200

    Решение задачи 1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -14,15 +14,16 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
         int age = 0;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age > 0) {
+            return "Вам меньше 0";
+        }
+        return "Вам 31 год";
     }
 }
