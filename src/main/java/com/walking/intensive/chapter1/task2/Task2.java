5313413c8ccc1623f72f0e1aa616e30d8837baae
Alex <muav1902@gmail.ru>
2024-11-17 19:41:29 +0300

    Сделал таск

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -34,10 +34,13 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+
+
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         //        Место для вашего кода
+
 
         return null; // Заглушка. При реализации - удалить
     }
