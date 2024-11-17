80629c8dc98c7ba4c7771125398a7fd2f73031e4
Asidorova <asidorova@diasoft.ru>
2024-11-17 14:47:34 +0500

    использование ввода с клавиатуры, вывода текста, if - elseif, switch-case

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,5 +1,6 @@
 package com.walking.intensive.chapter1.task1;
 
+import java.util.Scanner;
 /**
  * Реализуйте метод getAgeString(), который будет принимать параметром целое число (возраст) и возвращать строку
  * вида: "Вам N лет". Программа должна учитывать правила русского языка.
@@ -15,14 +16,43 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        Scanner scanner = new Scanner(System.in);
+        int age = scanner.nextInt();
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        String yeartext = "-1";
+        int ageunit = 1;
+        if (age >= 5 && age <= 20) {
+            yeartext = "лет";
+        } else if (age > 20 && age <= 100) {
+            ageunit = (age % 10);
+            switch (ageunit) {
+                case 0:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                    yeartext = "лет";
+                    break;
+                case 1:
+                    yeartext = "год";
+                    break;
+                case 2:
+                case 3:
+                case 4:
+                    yeartext = "года";
+                    break;
 
-        return null; // Заглушка. При реализации - удалить
+            }
+        }
+        if (yeartext == "-1") {
+            return "Некорректный ввод";
+        } else {
+            return "Вам " + age + " " + yeartext; // Заглушка. При реализации - удалить
+        }
     }
 }
