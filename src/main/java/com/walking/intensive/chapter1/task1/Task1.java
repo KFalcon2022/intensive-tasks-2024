28451d493a489849971041ea5082ce2a775b591c
alonecat13 <alonecat13@gmail.com>
2024-11-17 11:22:16 +0300

    Task 1 complete.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter1.task1;
 
+import javax.swing.*;
+
 /**
  * Реализуйте метод getAgeString(), который будет принимать параметром целое число (возраст) и возвращать строку
  * вида: "Вам N лет". Программа должна учитывать правила русского языка.
@@ -15,14 +17,29 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 8;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        String convertAgeToString = String.valueOf(age);
+        String AgeSuffix = "";
+
+        if (convertAgeToString.endsWith("1") == true && age != 11) {
+            AgeSuffix = "Вам " + age + " год";
+
+        } else if ((convertAgeToString.endsWith("2") == true || convertAgeToString.endsWith("3") == true || convertAgeToString.endsWith("4") == true) && age != 12 && age != 13 && age != 14) {
+            AgeSuffix = "Вам " + age + " года";
+
+        } else if (age < 0 || age > 120) {
+            AgeSuffix = "Так не бывает!!!";
+
+        } else {
+            AgeSuffix = "Вам " + age + " лет";
+        }
+
+        return AgeSuffix;
     }
 }
