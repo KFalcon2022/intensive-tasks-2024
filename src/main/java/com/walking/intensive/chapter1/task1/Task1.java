af064d192b84b446ee926d0d28a28cac848c87d9
i5t4b3r <i5t4b3r@gmail.com>
2024-11-18 12:31:44 +0300

    end task 1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -14,15 +14,28 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 2;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
+        String answer = "";
 
-        return null; // Заглушка. При реализации - удалить
+        if (age > -1) {
+            if (age % 10 > 4 || age % 10 == 0) {
+                answer = "Вам " + age + " лет";
+            } else if (age % 100 > 10 && age % 100 < 20) {
+                answer = "Вам " + age + " лет";
+            } else if (age % 10 >= 2) {
+                answer = "Вам " + age + " года";
+            } else {
+                answer = "Вам " + age + " год";
+            }
+        } else {
+            answer = "Некорректный ввод";
+        }
+
+        return answer;
     }
 }
