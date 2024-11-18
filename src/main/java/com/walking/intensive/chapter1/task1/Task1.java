23f67f95498d7beef12184d5a00a02ac24bdb40a
Ничоси-ПС <lana356rcv@gmail.com>
2024-11-18 18:43:41 +0700

    Задание1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,28 +1,28 @@
 package com.walking.intensive.chapter1.task1;
 
-/**
- * Реализуйте метод getAgeString(), который будет принимать параметром целое число (возраст) и возвращать строку
- * вида: "Вам N лет". Программа должна учитывать правила русского языка.
- *
- * <p>Пример верного исполнения кода: "Вам 5 лет", "Вам 4 года".
- * Пример неверной работы программы: "Вам 14 года".
- *
- * <p>Также необходимо учесть негативные сценарии. Например, попытку передать в метод невалидное значение - например,
- * отрицательное число. В таких случаях ожидается, что метод вернет строку "Некорректный ввод".
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
- */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+
+        int age = 12;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (age >= 0) {
+            String result = "Вам " + age;
+            if (age % 10 == 1 && age != 11) {
+                result = result + " год";
+            } else if ((age % 10 == 2 || age % 10 == 3 || age % 10 == 4) && age != 12 && age != 13 && age != 14) {
+                result = result + " года";
+            } else {
+                result = result + " лет";
+            }
+            return result;
+        } else {
+            return "Некорректный ввод";
+        }
+
     }
 }
