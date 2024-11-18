d5a31770a3908b2b42b8f1cd8654c6b12c3c8d78
Roman <ivashovroman96@mail.ru>
2024-11-18 21:41:50 +0400

    My solution

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,14 +15,20 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 14;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age % 10 == 0 || (age > 10 && age < 15) || age % 10 > 4 || (age > 110 && age < 115)) {
+            return "Вам " + age + " лет";
+        } else if (age % 10 == 1) {
+            return "Вам " + age + " год";
+        } else if (age % 10 > 1 && age % 10 < 5) {
+            return "Вам " + age + " года";
+        } else {
+            return "Некорректный ввод";
+        }
     }
 }
