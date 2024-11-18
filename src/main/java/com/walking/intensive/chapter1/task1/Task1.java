c63b0857cbb202678c875d070ae61f23f63b2951
Anton <goptarevanton@gmail.com>
2024-11-18 19:24:41 +0300

    task1. Done!

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -14,15 +14,25 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
         int age = 0;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if ((age % 10) == 1 && (age % 100) != 11) {
+            return "Вам " + age + " год";
+        } else if (age == 0 || (age >= 10 && age <= 20) || ((age % 100) >= 10 && (age % 100) <= 20)) {
+            return "Вам " + age + " лет";
+        }else if ((age % 10) > 1 && (age % 10) < 5) {
+            return "Вам " + age + " года";
+        } else if (age > 4) {
+            return "Вам " + age + " лет";
+        } else {
+            return "Некорректный ввод";
+        }
+
     }
 }
