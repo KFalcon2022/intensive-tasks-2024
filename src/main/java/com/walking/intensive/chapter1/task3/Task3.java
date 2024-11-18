b87e595ea0aee978e1b805e32233b00600f5f65b
i.baykalov <ilyabaikalov@gmail.com>
2024-11-18 10:31:42 +0300

    Task3 fix

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -33,14 +33,7 @@ public class Task3 {
         if (year < 1) {
             return false;
         }
-        if (year % 400 == 0) {
-            return true;
-        } else if (year % 100 == 0) {
-            return false;
-        } else if (year % 4 == 0) {
-            return true;
-        } else {
-            return false;
-        }
+
+        return year % 400 == 0 || (year % 100 != 0 && year % 4 == 0);
     }
 }
