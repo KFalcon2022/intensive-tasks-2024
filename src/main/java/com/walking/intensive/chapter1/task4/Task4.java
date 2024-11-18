fc576ca7d5e6f15ffbf9563fd43b2ccf44bd5bed
Dinara0113 <dinarkatttb16@gmail.com>
2024-11-18 21:06:41 +0500

    ch-1 task 4 commit

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -28,14 +28,42 @@ public class Task4 {
         double a = 0;
         double b = 0;
         double c = 0;
-
-        System.out.println(solveEquation(a, b, c));
-
+        System.out.println(solveEquation(5, -2, 1));
     }
 
     static String solveEquation(double a, double b, double c) {
         //        Место для вашего кода
+        if (a == 0) {
+            if (b == 0) {
+                if (c == 0) {
+                    return "Бесконечное множество решений.";
+                } else {
+                    return "Количество решений: 0.";
+                }
+            } else {
+                double root = -c / b;
+                return String.format("Количество решений: 1. Корень: %.6f", root);
+            }
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            double sqrtD = Math.sqrt(discriminant);
+            double x1 = (-b + sqrtD) / (2 * a);
+            double x2 = (-b - sqrtD) / (2 * a);
+
+            if (x1 > x2) {
+                double temp = x1;
+                x1 = x2;
+                x2 = temp;
+            }
+            return String.format("Количество решений: 2. Корни: %.6f;%.6f", x1, x2);
+        } else if (discriminant == 0) {
+            double x = -b / (2 * a);
+            return String.format("Количество решений: 1. Корень: %.6f", x);
+        } else {
+            return "Количество решений: 0.";
+        }
     }
 }
