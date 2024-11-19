005d9516ea9d6673eb92f0a88005ebf42b34a6f7
aleksandrsidelynikov <aleksandr.sidelynikov@gmail.com>
2024-11-19 22:21:22 +0300

    Реализован ввод возраста Реализован вывод текста для всех возрастов Реализован вывод некорректных значений

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -2,37 +2,50 @@ package com.walking.intensive.chapter1.task1;
 
 import java.util.Scanner;
 
-/**
- * Реализуйте метод getAgeString(), который будет принимать параметром целое число (возраст) и возвращать строку
- * вида: "Вам N лет". Программа должна учитывать правила русского языка.
- *
- * <p>Пример верного исполнения кода: "Вам 5 лет", "Вам 4 года".
- * Пример неверной работы программы: "Вам 14 года".
- *
- * <p>Также необходимо учесть негативные сценарии. Например, попытку передать в метод невалидное значение - например,
- * отрицательное число. В таких случаях ожидается, что метод вернет строку "Некорректный ввод".
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
- */
-
 public class Task1 {
     public static void main(String[] args) {
 
-// ввод возраста
         Scanner in = new Scanner(System.in);
-        System.out.print("Введите возраст: ");
+        System.out.print("Введите свой возраст: ");
         int age = in.nextInt();
+
         {
-            switch (age){
-                case 0:
-                    System.out.println("Вам " + age + " лет");
-                    break;
-                case 1:
-                    System.out.println("Вам " + age + " год");
-                    break;
-                case 2:
-                    System.out.println("Вам " + age + " года");
-                    break;
+            if (age <= 0) {
+                System.out.println("Некорректный ввод");
+            }
+            else {
+                switch (age % 10) {
+                    case 0:
+                        System.out.println("Вам " + age + " лет");
+                        break;
+                    case 1:
+                        System.out.println("Вам " + age + " год");
+                        break;
+                    case 2:
+                        System.out.println("Вам " + age + " года");
+                        break;
+                    case 3:
+                        System.out.println("Вам " + age + " года");
+                        break;
+                    case 4:
+                        System.out.println("Вам " + age + " года");
+                        break;
+                    case 5:
+                        System.out.println("Вам " + age + " лет");
+                        break;
+                    case 6:
+                        System.out.println("Вам " + age + " лет");
+                        break;
+                    case 7:
+                        System.out.println("Вам " + age + " лет");
+                        break;
+                    case 8:
+                        System.out.println("Вам " + age + " лет");
+                        break;
+                    case 9:
+                        System.out.println("Вам " + age + " лет");
+                        break;
+                }
             }
         }
         in.close();
