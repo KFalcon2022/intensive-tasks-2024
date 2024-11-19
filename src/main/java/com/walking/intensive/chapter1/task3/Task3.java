5f5adf7d5f87c662d1d420de9c9b93e0404dd802
r0nery <romahasla@gmail.com>
2024-11-19 06:53:56 +0300

    task3 ready

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -26,11 +26,23 @@ package com.walking.intensive.chapter1.task3;
 public class Task3 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(isLeap(1998));
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
 
-        return false; // Заглушка. При реализации - удалить
+        if (year < 0) {
+            return false;
+        }
+
+        if (year % 400 == 0) {
+            return true;
+        }
+
+        if(year % 100 == 0) {
+            return false;
+        }
+
+        return year % 4 == 0;
     }
 }
