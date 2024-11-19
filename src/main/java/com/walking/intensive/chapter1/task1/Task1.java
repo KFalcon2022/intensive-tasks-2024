035de65caaad3c609b51fe5e17af7ee20029d182
rekynkovav <rekynkovav@yandex.ru>
2024-11-19 13:57:13 +0300

    готовый вариант

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -14,15 +14,21 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
 
-        System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age <= 0) {
+            return "Вы ввели не корректную дату";
+        }
+        if (age == 1 | (age > 20 && age % 10 == 1)) {
+            return "Вам " + age + " год";
+        }
+        if (age < 5 | (age > 20 & age % 10 == 2 | age % 10 == 3 | age % 10 == 4)) {
+            return "Вам " + age + " года";
+        } else {
+            return "Вам " + age + " лет";
+        }
     }
 }
+
