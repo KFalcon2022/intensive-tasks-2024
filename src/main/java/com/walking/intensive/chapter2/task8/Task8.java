56e0d3ec172b6316fb17ab484c75edd1bdc4094d
AlGar <2677766@mail.ru>
2024-11-19 23:01:01 +1000

    Task8 Solution

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter2.task8;
 
+import java.util.Arrays;
+
 /**
  * Добираясь в школу на трамвае Вова проверяет, является ли купленный билет счастливым.
  * Если является, то нужно загадать желание и съесть билетик.
@@ -20,11 +22,42 @@ package com.walking.intensive.chapter2.task8;
  */
 public class Task8 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        int[] sumFrequency = new int[28];
+
+        for (int i = 0; i < 1000; i++) {
+            int sum = (i / 100) + (i / 10 % 10) + (i % 10);
+            sumFrequency[sum]++;
+//            [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 63, 69, 73, 75, 75, 73, 69, 63, 55, 45, 36, 28, 21, 15, 10, 6, 3, 1]
+        }
+        System.out.println(Arrays.toString(sumFrequency));
     }
 
     static double getHappyTicketChance() {
-        // Ваш код
-        return 0.0;
+        int totalTickets = 1_000_000;
+
+        int[] sumFrequency = calculateSumFrequency();
+
+        int happyTickets = calculateHappyTickets(sumFrequency);
+
+        return (double) happyTickets / totalTickets;
+    }
+
+    private static int[] calculateSumFrequency() {
+        int[] sumFrequency = new int[28];
+
+        for (int i = 0; i < 1000; i++) {
+            int sum = (i / 100) + (i / 10 % 10) + (i % 10);
+            sumFrequency[sum]++;
+        }
+        return sumFrequency;
+    }
+
+    private static int calculateHappyTickets(int[] sumFrequency) {
+        int happyTickets = 0;
+
+        for (int sum = 0; sum <= 27; sum++) {
+            happyTickets += sumFrequency[sum] * sumFrequency[sum];
+        }
+        return happyTickets;
     }
 }
