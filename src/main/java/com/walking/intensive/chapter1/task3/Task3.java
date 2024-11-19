b1eacc35b814c6083de846783f163ded5f83842c
Evgencms <prcev888@rambler.ru>
2024-11-19 10:09:13 +0300

    Исправил. В return добавил логическое условие, удалил переменную.

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -31,8 +31,6 @@ public class Task3 {
 
     static boolean isLeap(int year) {
 
-        boolean isLeapYear = year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
-
-        return year > 0 && isLeapYear;
+        return year > 0 && year % 4 == 0 && year % 100 != 0 || year % 400 == 0;
     }
 }
