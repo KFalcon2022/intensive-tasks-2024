3e2fe662bafdeeaa506218416c6c00993c6e885b
i5t4b3r <i5t4b3r@gmail.com>
2024-11-19 10:28:28 +0300

    fix: delete if\else

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,8 +29,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        if(year % 400 == 0) {
-            return true;
-        } else return year % 4 == 0 && year % 100 != 0;
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
     }
 }
