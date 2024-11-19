1c34e92fc645bb229f7db5b60eaef29dbe01b57b
Roman <ivashovroman96@mail.ru>
2024-11-19 23:09:19 +0400

    1. Вынес проверку на отрицательное значение age в отдельный if в начало метода. 2. Убрал return null;.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,21 +15,22 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 44;
+        int age = 344;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        if (age % 10 == 0 || (age > 10 && age < 15) || age % 10 > 4 || (age > 110 && age < 115)) {
-            return "Вам " + age + " лет";
-        } else if (age % 10 == 1) {
-            return "Вам " + age + " год";
-        } else if (age % 10 > 1 && age % 10 < 5) {
-            return "Вам " + age + " года";
-        } else if (age < 0) {
+        if (age < 0) {
             return "Некорректный ввод";
+        } else {
+            if (age % 10 == 0 || (age > 10 && age < 15) || age % 10 > 4 || (age > 110 && age < 115)) {
+                return "Вам " + age + " лет";
+            } else if (age % 10 == 1) {
+                return "Вам " + age + " год";
+            } else {
+                return "Вам " + age + " года";
+            }
         }
-    return null;
     }
 }
