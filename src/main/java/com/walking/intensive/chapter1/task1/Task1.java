bd8d369aec7ff471ed8a0ae15e7984285e59a2e9
igorek <seven65@yandex.ru>
2024-11-19 23:04:11 +0300

    Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,21 +15,25 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 112;
+        int age = 22;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        if (age >= 11 && age <= 14 || age >= 111 && age <= 114 || age % 10 == 5 || age % 10 == 6 || age % 10 == 7 || age % 10 == 8 || age % 10 == 9 || age % 10 == 0) {
+        if (age < 0 || age > 127) {
+            return "Некорректный ввод";
+        }
+        if (age % 100 >= 11 && age % 100 <= 14 || age % 10 >= 5 && age % 10 <= 9 || age % 10 == 0) {
             return "Вам " + age + " лет";
         }
         if (age % 10 == 1) {
             return "Вам " + age + " год";
         }
-        if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4) {
+        if (age % 10 >= 2 && age % 10 <= 4) {
             return "Вам " + age + " года";
         }
+
         return "Некорректный ввод";
     }
 }
