cb1e39510313bfb4b7096530b3d3fa6296801cb7
Ничоси-ПС <lana356rcv@gmail.com>
2024-11-19 20:47:16 +0700

    Задание2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -2,7 +2,7 @@ package com.walking.intensive.chapter1.task2;
 
 public class Task2 {
     public static void main(String[] args) {
-        System.out.println(getFlatLocation(10, 3, 40));
+        System.out.println(getFlatLocation(10, 3, 0));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
