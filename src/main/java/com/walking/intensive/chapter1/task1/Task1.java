4d62c6bc8a295f90eb6e453abbf32e4f46894992
alonecat13 <alonecat13@gmail.com>
2024-11-19 21:54:07 +0300

    Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -17,17 +17,18 @@ import javax.swing.*;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 100;
-
+        int age = 110;
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
         if (age < 0 || age > 120) {
             return "Так не бывает!!!";
-        } else if (((age >= 2 && age <= 4) || (age % 10 >= 2 && age % 10 <= 4)) && (age != 12 && age != 13 && age != 14)) {
+        }
+        if (((age >= 2 && age <= 4) || (age % 10 >= 2 && age % 10 <= 4)) && (age != 12 && age != 13 && age != 14)) {
             return "Vam " + age + " goda";
-        } else if ((age == 1 || age % 10 == 1) && (age % 100 != 11)) {
+        }
+        if ((age == 1 || age % 10 == 1) && (age % 100 != 11)) {
             return "Vam " + age + " god";
         }
         return "Vam " + age + " let";
