2a693aa163c9c4ee55ec89a54226337232ec1785
Roman <ivashovroman96@mail.ru>
2024-11-19 17:46:47 +0400

    Добавил проверку на ввод отрицательных чисел.

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,7 +15,7 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = -1;
 
         System.out.println(getAgeString(age));
     }
@@ -27,6 +27,8 @@ public class Task1 {
             return "Вам " + age + " год";
         } else if (age % 10 > 1 && age % 10 < 5) {
             return "Вам " + age + " года";
+        } else if (age < 0) {
+            return "Некорректный ввод";
         } else {
             return "Некорректный ввод";
         }
