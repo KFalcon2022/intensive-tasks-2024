0df625f06e5c36c70922f8fbb2f16257d05e041f
Dima Manshyn <dimendroidg@gmail.com>
2024-11-19 18:55:07 +0300

    Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -24,18 +24,44 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
-
-        System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        }
+        if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        }
+        if (a == 0 && b != 0) {
+            double x = (-c / b);
+            return "Количество решений: 1. Корень: " + correctValue(x);
+        }
+        double discriminant = (b * b) - (4 * a * c);
+        if (discriminant < 0) {
+            return "Количество решений: 0.";
+        } else if (discriminant == 0) {
+            double x = (-b / (2 * a));
+            return "Количество решений: 1. Корень: " + correctValue(x);
+        }
+        return getTwoRoots(a, b, discriminant);
+    }
 
-        return null; // Заглушка. При реализации - удалить
+
+    private static String getTwoRoots(double a, double b, double discriminant) {
+        double x1 = ((-b + Math.sqrt(discriminant)) / (2 * a));
+        double x2 = ((-b - Math.sqrt(discriminant)) / (2 * a));
+        if (x1 < x2) {
+            return "Количество решений: 2. Корни: " + x1 + ";" + x2;
+        } else {
+            return "Количество решений: 2. Корни: " + x2 + ";" + x1;
+        }
+    }
+
+    private static double correctValue(double x) {
+        if (x == -0.0) {
+            return 0.0;
+        }
+        return x;
     }
 }
