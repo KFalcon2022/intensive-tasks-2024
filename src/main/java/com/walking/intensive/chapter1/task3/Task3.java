f26c708bdc620983d8191ec23a3cf1c75ff20cb4
TOP1 <overseo4@gmail.com>
2024-11-19 23:35:45 +0500

    Убраны лишние скобки

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -32,6 +32,6 @@ public class Task3 {
     static boolean isLeap(int year) {
 
 
-        return year > 0 && (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0));
+        return year > 0 && (year % 400 == 0 || year % 4 == 0 && year % 100 != 0);
     }
 }
