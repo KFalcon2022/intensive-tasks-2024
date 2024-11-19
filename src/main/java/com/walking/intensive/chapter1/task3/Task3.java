3e3c95b73e425dfc20885096357727383022be28
Kot <kot@mailforgit.qzaaa>
2024-11-19 13:52:01 +0300

    Task3 убраны скобки и лишние строки

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -26,12 +26,10 @@ package com.walking.intensive.chapter1.task3;
 public class Task3 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-
     }
 
     static boolean isLeap(int year) {
         //Место для вашего кода
-        return (year % 400 == 0) || (year % 4 == 0 && year % 100 != 0);
-
+        return year % 400 == 0 || year % 4 == 0 && year % 100 != 0;
     }
 }
