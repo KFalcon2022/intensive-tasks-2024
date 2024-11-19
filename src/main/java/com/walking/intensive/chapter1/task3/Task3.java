82911fbfabf853633a537e67cc3835abf052df86
egor.djun <xedshotg@gmail.com>
2024-11-19 11:30:10 +0700

    task 3 fixed

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -33,10 +33,6 @@ public class Task3 {
             return false;
         }
 
-        if (year < 1582) { //юлианский
-            return year % 100 == 0 && year % 400 != 0;
-        } else {    //григорианский
-            return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
-        }
+        return (year % 4 == 0 && year % 100 != 0) || year % 400 == 0;
     }
 }
