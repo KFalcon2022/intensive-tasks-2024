14b2f2d73baaad1977dc80659a154c9b9263e328
Yuri <zembekov.m@gmail.com>
2024-11-19 10:09:43 +0300

    task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -16,13 +16,25 @@ public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         int age = 0;
-
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age < 0) {
+            return "Некорректный ввод";
+        } else {
+            int remainderOfDivideByTen = age % 10;
+            int remainderOfDivideByOneHundred = age % 100;
+            if ((remainderOfDivideByOneHundred > 10 && remainderOfDivideByOneHundred < 15) ||
+                    remainderOfDivideByTen == 0 || remainderOfDivideByTen > 4) {
+                return "Вам " + age + " лет";
+            } else {
+                if (remainderOfDivideByTen == 1) {
+                    return "Вам " + age + " год";
+                } else {
+                    return "Вам " + age + " года";
+                }
+            }
+        }
     }
 }
