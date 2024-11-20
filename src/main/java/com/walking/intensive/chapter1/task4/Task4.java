8412191ba3e2d1b161b7e859e953611768367456
Dima Manshyn <dimendroidg@gmail.com>
2024-11-20 19:11:01 +0300

    Task4 удалены лишние скобки, добавлены отступы для читаемости,else if заменен на if для последнего if в методе,заменены операторы if на тернарный оператор.

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -30,38 +30,38 @@ public class Task4 {
         if (a == 0 && b == 0 && c == 0) {
             return "Бесконечное множество решений.";
         }
+
         if (a == 0 && b == 0) {
             return "Количество решений: 0.";
         }
+
         if (a == 0 && b != 0) {
-            double x = (-c / b);
-            return "Количество решений: 1. Корень: " + correctValue(x);
+            double x = -c / b;
+            return "Количество решений: 1. Корень: " + getCorrectValue(x);
         }
-        double discriminant = (b * b) - (4 * a * c);
+
+        double discriminant = b * b - 4 * a * c;
+
         if (discriminant < 0) {
             return "Количество решений: 0.";
-        } else if (discriminant == 0) {
-            double x = (-b / (2 * a));
-            return "Количество решений: 1. Корень: " + correctValue(x);
+        }
+
+        if (discriminant == 0) {
+            double x = -b / 2 * a;
+            return "Количество решений: 1. Корень: " + getCorrectValue(x);
         }
         return getTwoRoots(a, b, discriminant);
     }
 
 
     private static String getTwoRoots(double a, double b, double discriminant) {
-        double x1 = ((-b + Math.sqrt(discriminant)) / (2 * a));
-        double x2 = ((-b - Math.sqrt(discriminant)) / (2 * a));
-        if (x1 < x2) {
-            return "Количество решений: 2. Корни: " + x1 + ";" + x2;
-        } else {
-            return "Количество решений: 2. Корни: " + x2 + ";" + x1;
-        }
+        double x1 = (-b + Math.sqrt(discriminant)) / 2 * a;
+        double x2 = (-b - Math.sqrt(discriminant)) / 2 * a;
+        return "Количество решений: 2. Корни: " +
+                (x1 < x2 ? x1 + ";" + x2 : x2 + ";" + x1);
     }
 
-    private static double correctValue(double x) {
-        if (x == -0.0) {
-            return 0.0;
-        }
-        return x;
+    private static double getCorrectValue(double x) {
+        return x == -0.0 ? 0.0 : x;
     }
 }
