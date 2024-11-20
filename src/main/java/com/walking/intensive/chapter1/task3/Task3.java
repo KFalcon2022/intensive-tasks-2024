839250ad3f53facf381074e1313d19ee0ec1e260
Roman <ivashovroman96@mail.ru>
2024-11-20 22:52:14 +0400

    Task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -33,11 +33,7 @@ public class Task3 {
         }
         if (year % 4 == 0) {
             if (year % 100 == 0) {
-                if (year % 400 == 0) {
-                    return true;
-                } else {
-                    return false;
-                }
+                return year % 400 == 0;
             } else {
                 return true;
             }
