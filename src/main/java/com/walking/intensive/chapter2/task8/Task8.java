1b7e872b831cd7005dd5438ae02af18ca132c2a2
AlGar <2677766@mail.ru>
2024-11-20 10:39:18 +1000

    Task8 Solution

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -21,43 +21,63 @@ import java.util.Arrays;
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
 public class Task8 {
+
     public static void main(String[] args) {
         int[] sumFrequency = new int[28];
 
-        for (int i = 0; i < 1000; i++) {
-            int sum = (i / 100) + (i / 10 % 10) + (i % 10);
+        for (int i = 0; i < 1000 ; i++) {
+            int sum = i / 100 + i / 10 % 10 + i % 10;
             sumFrequency[sum]++;
-//            [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 63, 69, 73, 75, 75, 73, 69, 63, 55, 45, 36, 28, 21, 15, 10, 6, 3, 1]
         }
+
         System.out.println(Arrays.toString(sumFrequency));
+//        [1, 3, 6, 10, 15, 21, 28, 36, 45, 55, 63, 69, 73, 75, 75, 73, 69, 63, 55, 45, 36, 28, 21, 15, 10, 6, 3, 1]
+
+        System.out.println(getHappyTicketChance());
     }
 
     static double getHappyTicketChance() {
-        int totalTickets = 1_000_000;
+        final int ticketsAmount = 1_000_000;
 
         int[] sumFrequency = calculateSumFrequency();
 
         int happyTickets = calculateHappyTickets(sumFrequency);
 
-        return (double) happyTickets / totalTickets;
+        return (double) happyTickets / ticketsAmount;
     }
 
     private static int[] calculateSumFrequency() {
-        int[] sumFrequency = new int[28];
+        final int maxSumThreeDigits = 9 + 9 + 9;
+        final int threeDigitRangeLimit = 1000;
 
-        for (int i = 0; i < 1000; i++) {
-            int sum = (i / 100) + (i / 10 % 10) + (i % 10);
+        int[] sumFrequency = new int[maxSumThreeDigits + 1];
+
+        for (int i = 0; i < threeDigitRangeLimit; i++) {
+            int sum = calculateDigitSum(i);
             sumFrequency[sum]++;
         }
+
         return sumFrequency;
+    }
+
+    private static int calculateDigitSum(int number) {
+        int sum = 0;
+
+        while (number > 0) {
+            sum += number % 10;
+            number /= 10;
+        }
+
+        return sum;
     }
 
     private static int calculateHappyTickets(int[] sumFrequency) {
         int happyTickets = 0;
 
-        for (int sum = 0; sum <= 27; sum++) {
-            happyTickets += sumFrequency[sum] * sumFrequency[sum];
+        for (int sum : sumFrequency) {
+            happyTickets += sum * sum;
         }
+
         return happyTickets;
     }
 }
