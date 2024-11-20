709ed94ce028352f03aefb644889e87269642677
Dinara0113 <dinarkatttb16@gmail.com>
2024-11-20 18:20:00 +0500

    ch-1 task 1 new commit

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,18 +15,26 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 5;
+        int age = 1;
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 //        Место для вашего кода
         int lastNumber = age % 10;
-        if (age < 0) {
+        int lastTwoNumbers = age % 100;
+        if (age < 0 || age > 127) {
             return "Некорректный ввод";
-        } else if (lastNumber >= 2 && lastNumber <= 4) {
+        }
+
+        if (lastNumber == 1 && lastTwoNumbers != 11) {
+            return "Вам " + age + " год";
+        }
+
+        if (lastNumber >= 2 && lastNumber <= 4 && (lastTwoNumbers < 12 || lastTwoNumbers > 14)) {
             return "Вам " + age + " года";
-        } else return "Вам " + age + " лет";
+        }
+        return "Вам " + age + " лет";
     }
 }
 
