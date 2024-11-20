f6cfe65fd1725733a8d94cfb5b1c3df7fa680688
alexa <alexa.markova@outlook.com>
2024-11-20 17:11:53 +0300

    commit task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -14,15 +14,42 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+
+        int age = -111;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        String baseString = "Вам " + age;
+
+        return switch (getAgeParam(age)) {
+            case 1:
+                yield baseString + " год";
+            case 2, 3, 4:
+                yield baseString + " года";
+            case 5, 6, 7, 8, 9, 0:
+                yield baseString + " лет";
+            default:
+                yield "Некорректный ввод";
+        };
+
+    }
+
+    static int getAgeParam(int age) {
+
+        if (age >= 0) {
+
+            int ageParam = (age - (age / 100) * 100);
+
+            if (ageParam >= 11 & ageParam <= 14) {
+                ageParam = 0;
+            } else {
+                ageParam = age - (age / 10) * 10;
+            }
+            return ageParam;
+        }
+        return age;
     }
 }
