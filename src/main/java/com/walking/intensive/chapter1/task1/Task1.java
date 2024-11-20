40c07ca84862ffee4c045d436852425aa1dd7ea0
Ilgiz <s.ilgiz777@gmail.com>
2024-11-21 00:12:19 +0500

    Сколько лет, по посл. цифре и предпоследней

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,20 +15,21 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
+        int age = 112;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 
+        int lastDigit = age % 10;
+        int preLastDigit = (age / 10) % 10;
+        String ending = "лет";
         if (age < 0) {
             return "Некорректный ввод";
-        }
-
-        String ending = "лет";
-        int lastDigit = age % 10;
-        if (lastDigit > 0 && lastDigit <= 4) {
+        } else if (lastDigit == 1 && preLastDigit != 1) {
+            ending = "год";
+        } else if ((lastDigit == 2 || lastDigit == 3 || lastDigit == 4) && preLastDigit != 1) {
             ending = "года";
         }
         return "Вам " + age + " " + ending;
