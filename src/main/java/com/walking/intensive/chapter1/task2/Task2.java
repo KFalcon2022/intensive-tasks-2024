e3ddd9c59089e5251f595542cae9652cf4c596b3
Evgencms <prcev888@rambler.ru>
2024-11-20 16:42:37 +0300

    Выполнено задание на определение подъезда, этажа и расположения нужной квартиры относительно лифта.

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -33,12 +33,52 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        System.out.println(getFlatLocation(10, 3, 88));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        boolean isNonExistentFlat = floorAmount * entranceAmount * 4 < flatNumber;
+        boolean isNegativeNumber = floorAmount < 0 || entranceAmount < 0 || flatNumber < 0;
+        boolean isZero = floorAmount == 0 || entranceAmount == 0 || flatNumber == 0;
+
+        int entranceNumber = (flatNumber - 1) / (floorAmount * 4) + 1;
+        int floorNumber = (flatNumber - 1) % (floorAmount * 4) / 4 + 1;
+
+        if (isNonExistentFlat) {
+            return "Такой квартиры не существует";
+        }
+
+        if (isNegativeNumber || isZero) {
+            return "Некорректные входные данные";
+        }
+
+        if (flatNumber % 4 == 1) {
+            return flatNumber + " кв - " + entranceNumber + " подъезд, "
+                    + floorNumber + " этаж, слева от лифта, влево";
+        }
+
+        if (flatNumber % 4 == 2) {
+            return flatNumber + " кв - " + entranceNumber + " подъезд, "
+                    + floorNumber + " этаж, слева от лифта, вправо";
+        }
+
+        if (flatNumber % 4 == 3) {
+            return flatNumber + " кв - " + entranceNumber + " подъезд, "
+                    + floorNumber + " этаж, справа от лифта, влево";
+        }
+
+        if (flatNumber % 4 == 4) {
+            return flatNumber + " кв - " + entranceNumber + " подъезд, "
+                    + floorNumber + " этаж, справа от лифта, влево";
+        }
+
+        if (flatNumber % 4 + 4 == 4) {
+            return flatNumber + " кв - " + entranceNumber + " подъезд, "
+                    + floorNumber + " этаж, справа от лифта, вправо";
+        }
+
+        return "Некорректные входные данные";
     }
 }
