c9b4dd74fdab9a713fb559983fa261ef4b081072
Гепанов Антон <anton.hepanau@softclub.by>
2024-11-20 13:28:52 +0300

    Task4 commit

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,8 +26,8 @@ public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         double a = 1;
-        double b = -4;
-        double c = 4;
+        double b = 0;
+        double c = 0;
 
         System.out.println(solveEquation(a, b, c));
 
@@ -38,26 +38,43 @@ public class Task4 {
         double x1 = 0.0;
         double x2 = 0.0;
         double x = 0.0;
-        double d;
-        d = (b * b) - ((4 * a) * c);
+        double d = (b * b) - ((4 * a) * c);
+
+        if (a == 0 & c == 0) {
+            return "Бесконечное множество решений.";
+        }
+        if (a == 0 & b == 0) {
+            return "Количество решений: 0.";
+        }
+        if (b == 0 & c == 0) {
+            x = 0;
+            return "Количество решений: 1. " + "Корень: " + x;
+        }
+        if (a == 0) {
+            x = -c / b;
+            return "Количество решений: 1. " + "Корень: " + x;
+        }
+
         // x1 / x2 = -b / a;
         // x2 * x2 = c / a;
 
+        if (d == 0) {
+            x = -b / (2 * a);
+            return "Количество решений: 1. " + "Корень: " + x;
+        }
         if (d > 0) {
             double e = Math.sqrt(d);
             x1 = (-b + e) / (2 * a);
             x2 = (-b - e) / (2 * a);
             if (x1 > x2) {
-                return d + "Количество решений: 2." + " Корни: " + x2 + "; " + x1;
-            } else {
-                return d + "Количество решений: 2." + " Корни: " + x1 + "; " + x2;
+                return "Количество решений: 2." + " Корни: " + x2 + ";" + x1;
             }
-
-        } else if (d == 0) {
-            x = -b / 2 * a;
-            return d + "количество решений: 1 " + x;
+            {
+                return "Количество решений: 2." + " Корни: " + x1 + ";" + x2;
+            }
         }
-
-        return d + "нет корней";
+        {
+            return "Количество решений: 0.";
+        }
     }
 }
