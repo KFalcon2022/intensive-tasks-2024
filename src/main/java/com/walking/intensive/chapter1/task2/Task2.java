32b2712f24295d55ca5dadedc40d6df84b322874
Deoshor <deoshor@gmail.com>
2024-11-20 18:18:15 +1000

    Task 2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -33,12 +33,85 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        String result = getFlatLocation(2, 2, 9);
+        System.out.println(result);
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int countOfFlats = floorAmount * 4 * entranceAmount;
+        if (floorAmount <= 0 || entranceAmount <= 0 || flatNumber <= 0) {
+            return "Некорректные входные данные";
+        } else if (flatNumber > countOfFlats) {
+            return "Такой квартиры не существует";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        int countOfFlatsInEntrance = countOfFlats / entranceAmount;
+        int entrance = getEntrance(flatNumber, entranceAmount, countOfFlatsInEntrance);
+        int floor = getFloor(flatNumber, countOfFlatsInEntrance);
+        String direction = getDirection(flatNumber, countOfFlatsInEntrance);
+
+        return flatNumber + " кв - " + entrance + " подъезд, " + floor + " этаж, " + direction;
+    }
+
+    private static int getEntrance(int flatNumber, int entranceAmount, int countOfFlatsInEntrance) {
+        int entrance = 1;
+        while (entrance <= entranceAmount) {
+            if (flatNumber <= countOfFlatsInEntrance) {
+                break;
+            } else {
+                entrance++;
+                if (flatNumber <= countOfFlatsInEntrance * entrance) {
+                    break;
+                }
+            }
+        }
+        return entrance;
+    }
+
+    private static int getFloor(int flatNumber, int countOfFlatsInEntrance) {
+        int floor = 1;
+        if (flatNumber < countOfFlatsInEntrance) {
+            if (flatNumber % 2 == 0) {
+                floor = flatNumber / 4;
+            } else {
+                floor += flatNumber / 4;
+            }
+        } else if (flatNumber == countOfFlatsInEntrance) {
+            floor = flatNumber / 4;
+        } else {
+            int numberOfFlatInEntrance = (flatNumber - ((flatNumber / countOfFlatsInEntrance)) * countOfFlatsInEntrance);
+            if (numberOfFlatInEntrance < countOfFlatsInEntrance) {
+                if (flatNumber % 2 == 0) {
+                    floor = numberOfFlatInEntrance / 4;
+                } else {
+                    floor += numberOfFlatInEntrance / 4;
+                }
+            } else if (numberOfFlatInEntrance == countOfFlatsInEntrance) {
+                floor = numberOfFlatInEntrance / 4;
+            }
+        }
+        return floor;
+    }
+
+    private static String getDirection(int flatNumber, int countOfFlatsInEntrance) {
+        String leftOfElevator = "слева от лифта";
+        String rightOfElevator = "справа от лифта";
+        String left = "влево";
+        String right = "вправо";
+
+        int direction;
+        if (flatNumber <= countOfFlatsInEntrance) {
+            direction = flatNumber % 4;
+        } else {
+            direction = (flatNumber - ((flatNumber / countOfFlatsInEntrance)) * countOfFlatsInEntrance) % 4;
+        }
+
+        return switch (direction) {
+            case 0 -> rightOfElevator + ", " + right;
+            case 1 -> leftOfElevator + ", " + left;
+            case 2 -> leftOfElevator + ", " + right;
+            case 3 -> rightOfElevator + ", " + left;
+            default -> throw new IllegalStateException("Некорректные входные данные");
+        };
     }
 }
