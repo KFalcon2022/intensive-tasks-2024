551ac3b1ddcb80f74110861f539345c37be89351
egor <georgi.xen@gmail.com>
2024-11-20 20:13:14 +0300

    Task2

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -1,5 +1,6 @@
 package com.walking.intensive.chapter1.task2;
 
+
 /**
  * Реализуйте метод getFlatLocation(), который будет принимать параметрами следующие данные:
  * <ul>
@@ -34,11 +35,60 @@ package com.walking.intensive.chapter1.task2;
 public class Task2 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+
+        System.out.println(getFlatLocation(10, 3, 40));
+        System.out.println(getFlatLocation(11, 2, 45));
+        System.out.println(getFlatLocation(4, 1, 2));
+        System.out.println(getFlatLocation(2, 40, 1234));
+        System.out.println(getFlatLocation(2, 40, -12));
+
+
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
         //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        int numberOfApartmentsPerFloor = 4;
+
+        String badResult = "Некорректные входные данные";
+        String badResult2 = "Такой квартиры не существует";
+
+        if (flatNumber < 1) {
+            return badResult;
+        }
+
+        if (flatNumber > numberOfApartmentsPerFloor * entranceAmount * floorAmount) {
+            return badResult2;
+        }
+
+        // Определяем подъезд
+        int determineTheEntrance = (flatNumber - 1) / (floorAmount * numberOfApartmentsPerFloor) + 1;
+
+        // Определяем этаж
+        int apartmentNumberRelativeToTheEntrance = (flatNumber - 1) % (floorAmount * numberOfApartmentsPerFloor);
+        int floorNumber = apartmentNumberRelativeToTheEntrance / numberOfApartmentsPerFloor + 1;
+
+        // Расположение этажа относительно лифта
+        int positionPerFloor = apartmentNumberRelativeToTheEntrance % numberOfApartmentsPerFloor;
+
+        String positionRelativeToTheElevator;
+
+        if (positionPerFloor < 2) {
+            positionRelativeToTheElevator = "слева от лифта";
+        } else {
+            positionRelativeToTheElevator = "справа от лифта";
+        }
+
+        String route;
+
+        if (positionPerFloor % 2 == 0) {
+            route = "влево";
+        } else {
+            route = "вправо";
+        }
+
+        return flatNumber + " кв" + " - " + determineTheEntrance + " подъезд, " + floorNumber +
+                " этаж, " + positionRelativeToTheElevator + ", " + route;
+
     }
 }
