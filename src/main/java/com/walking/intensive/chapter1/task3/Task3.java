d3961689f7c11c74668b8544863730b1d63c7d7b
Yuri <zembekov.m@gmail.com>
2024-11-20 19:34:46 +0300

    task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,8 +29,9 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        //Место для вашего кода
-
-        return false; // Заглушка. При реализации - удалить
+        if (year % 4 != 0) {
+            return false;
+        }
+        return year % 400 == 0 || year % 100 != 0;
     }
 }
