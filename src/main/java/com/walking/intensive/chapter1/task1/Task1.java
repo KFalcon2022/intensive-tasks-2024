e52886217474d7316ee129f55d9a7de300a31eff
Neverhood <just7500@gmail.com>
2024-11-21 22:13:03 +0800

    Task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -1,39 +1,31 @@
 package com.walking.intensive.chapter1.task1;
 
-/**
- * Реализуйте метод getAgeString(), который будет принимать параметром целое число (возраст) и возвращать строку
- * вида: "Вам N лет". Программа должна учитывать правила русского языка.
- *
- * <p>Пример верного исполнения кода: "Вам 5 лет", "Вам 4 года".
- * Пример неверной работы программы: "Вам 14 года".
- *
- * <p>Также необходимо учесть негативные сценарии. Например, попытку передать в метод невалидное значение - например,
- * отрицательное число. В таких случаях ожидается, что метод вернет строку "Некорректный ввод".
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
- */
 public class Task1 {
     public static void main(String[] args) {
 
-        int age = 123;
+        int age = 100;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
 
-        String textMessage = "Вам " + age;
+        if (age < 0 || age > 127) {
+            return "РќРµРєРѕСЂСЂРµРєС‚РЅС‹Р№ РІРІРѕРґ";
+        }
 
-        if (age <= 0) {
-            textMessage = "Вам нет и годика!";
-        } else if (age % 100 > 10 && age % 100 < 15) {
-            textMessage = textMessage + " лет";
-        } else if (age % 10 > 1 && age % 10 < 5) {
-            textMessage = textMessage + " года";
-        } else if (age % 10 == 1) {
-            textMessage = textMessage + " год";
-        } else textMessage = textMessage + " лет";
+        if (age % 100 > 10 && age % 100 < 20) {
+            return "Р’Р°Рј " + age + " Р»РµС‚";
+        }
 
-        return textMessage;
+        if (age % 10 > 1 && age % 10 < 5) {
+            return "Р’Р°Рј " + age + " РіРѕРґР°";
+        }
+
+        if (age % 10 == 1) {
+            return "Р’Р°Рј " + age + " РіРѕРґ";
+        }
+
+        return "Р’Р°Рј " + age + " Р»РµС‚";
     }
 }
