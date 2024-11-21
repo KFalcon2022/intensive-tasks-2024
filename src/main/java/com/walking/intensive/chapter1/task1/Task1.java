712c2b378bb8621593eeb5b6ae4c5adb0776e1ad
Ftronick <sapin.roman.s@gmail.com>
2024-11-21 18:23:34 +0300

    первый

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,7 +15,7 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 121;
+        int age = 125;
 
         System.out.println(getAgeString(age));
     }
@@ -30,6 +30,12 @@ public class Task1 {
 
         if (age < 0) {
             System.out.println(result);
+            return result;
+        }
+
+        if (age > 122) {
+            System.out.println(result);
+            return result;
         }
 
         if ((age >= 11 && age < 15) || (age % 100 >= 11 && age % 100 <= 15)) {
