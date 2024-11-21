eab2a4779c28f1f6376131c252780377e9cae300
Гепанов Антон <anton.hepanau@softclub.by>
2024-11-21 16:20:25 +0300

    Task5 commit (переименование сircRadius)

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -171,8 +171,8 @@ public class Task5 {
         if ((a > 0 && b > 0 && c > 0) && (a + b > c && a + c > b && c + b > a)) {
             double p = (a + b + c) / 2;
             double sTriangle = Math.sqrt(p * (p - a) * (p - b) * (p - c));
-            double r = (a * b * c) / (4 * sTriangle);
-            return r;
+            double circRadius = (a * b * c) / (4 * sTriangle);
+            return circRadius;
         }
         return -1;
     }
