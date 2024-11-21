cebb22122b7312696b988b61608e8781c3208879
Alex <muav1902@gmail.ru>
2024-11-21 23:07:06 +0300

    Заменил else-if, на if, переписал тело. Вывел корни по возростанию. Исправил имя переменной.

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -27,41 +27,48 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-        double a = -1;
-        double b = 3;
-        double c = 4;
+        double a = 0;
+        double b = 0;
+        double c = -10;
 
         System.out.println(solveEquation(a, b, c));
     }
 
     static String solveEquation(double a, double b, double c) {
-        double D = Math.pow(b, 2) - (4 * a * c);
 
-        if (a < 0 && b < 0 && c < 0) {
-            return "Некорректные входные данные";
-        } else if (a == 0 && b == 0 && c == 0) {
-            return "Бесконечное множество решений";
-        } else if (a == 0 && b != 0 && c != 0) {
-            double x = -c / b;
-            return "Количество решений: 1. Корень: " + x;
-        } else if (D > 0) {
-            double x1 = (-b + Math.sqrt(D)) / (2 * a);
-            double x2 = (-b - Math.sqrt(D)) / (2 * a);
-            if (Double.isNaN(x1) || Double.isNaN(x2) || Double.isInfinite(x1) || Double.isInfinite(x2)) {
-                return "Некорректные входные данные";
+        if (a == 0) {
+            if (b == 0 && c == 0) {
+                return "Бесконечное множество решений";
             }
-            if (x1 < x2) {
+            if (b == 0 && c != 0) {
+                return "Количество решений: 0";
+            }
+            if (b != 0) {
+                double x = -c / b;
+                return "Количество решений: 1. Корень: " + x;
+            }
+        }
+
+        if (a != 0) {
+            double d = Math.pow(b, 2) - (4 * a * c);
+
+            if (d > 0) {
+                double x1 = (-b + Math.sqrt(d)) / (2 * a);
+                double x2 = (-b - Math.sqrt(d)) / (2 * a);
+
+                if (x1 < x2) {
+                    return "Количество решений: 2. Корни: " + x1 + ";" + x2;
+                }
                 return "Количество решений: 2. Корни: " + x2 + ";" + x1;
             }
-            return "Количество решений: 2. Корни: " + x1 + ";" + x2;
-        } else if (D == 0) {
-            double x = -b / (2 * a);
-            if (Double.isNaN(x) || Double.isInfinite(x)) {
-                return "Некорректные входные данные";
+            if (d == 0) {
+                double x = -b / (2 * a);
+                return "Количество решений: 1. Корень: " + x;
             }
-            return "Количество решений: 1. Корень: " + x;
-        } else {
-            return "Количество решений: 0.";
+            if (d < 0) {
+                return "Количество решений: 0";
+            }
         }
+        return "Некорректные входные данные";
     }
 }
