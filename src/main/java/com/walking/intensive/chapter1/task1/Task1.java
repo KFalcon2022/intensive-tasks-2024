c2be6253b52d3913596e666e0684d2d9016b1180
Rita <margarita.mamatkazina@gmail.com>
2024-11-21 20:35:39 +0300

    Поняла...

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -27,64 +27,16 @@ public class Task1 {
 
     static String getAgeString(int age) {
 
-        if (age >= 0 && age < 128) {
+        if (age % 10 == 1) {
+            return "Вам " + age + " год";
 
-            switch (age) {
-                case 1:
-                case 21:
-                case 31:
-                case 41:
-                case 51:
-                case 61:
-                case 71:
-                case 81:
-                case 91:
-                case 101:
-                case 121:
-                    return "Вам " + age + " год";
+        }else if (age % 10 == 2 || age % 10 == 3 || age % 10 == 4) {
+            return "Вам " + age + " года";
 
-                case 2:
-                case 3:
-                case 4:
-                case 22:
-                case 23:
-                case 24:
-                case 32:
-                case 33:
-                case 34:
-                case 42:
-                case 43:
-                case 44:
-                case 52:
-                case 53:
-                case 54:
-                case 62:
-                case 63:
-                case 64:
-                case 72:
-                case 73:
-                case 74:
-                case 82:
-                case 83:
-                case 84:
-                case 92:
-                case 93:
-                case 94:
-                case 102:
-                case 103:
-                case 104:
-                case 122:
-                case 123:
-                case 124:
-                    return "Вам " + age + " года";
-
-                default:
+        }else if (age >= 0){
                     return "Вам " + age + " лет";
             }
 
-        }else{
-                return "Некорректный ввод";
-            }
-
+        return "Некорректный ввод";
     }
 }
