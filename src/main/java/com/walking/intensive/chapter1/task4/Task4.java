a57b25fb9226e1fcac6d2014b5956ad1b2519b08
Гепанов Антон <anton.hepanau@softclub.by>
2024-11-21 10:37:59 +0300

    Task4 commit (переменные объявляются перед использованием, удалены лишние {}, добавлено использование Math.min(), Ьath.max() )

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -25,19 +25,14 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 1;
-        double b = 0;
-        double c = 0;
-
+        double a = 2;
+        double b = 5;
+        double c = 3;
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
         //        Место для вашего кода
-        double x1 = 0.0;
-        double x2 = 0.0;
-        double x = 0.0;
         double d = (b * b) - ((4 * a) * c);
 
         if (a == 0 & c == 0) {
@@ -47,31 +42,23 @@ public class Task4 {
             return "Количество решений: 0.";
         }
         if (b == 0 & c == 0) {
-            x = 0;
+            double x = 0;
             return "Количество решений: 1. " + "Корень: " + x;
         }
         if (a == 0) {
-            x = -c / b;
+            double x = -c / b;
             return "Количество решений: 1. " + "Корень: " + x;
         }
-
         if (d == 0) {
-            x = -b / (2 * a);
+            double x = -b / (2 * a);
             return "Количество решений: 1. " + "Корень: " + x;
         }
         if (d > 0) {
             double e = Math.sqrt(d);
-            x1 = (-b + e) / (2 * a);
-            x2 = (-b - e) / (2 * a);
-            if (x1 > x2) {
-                return "Количество решений: 2." + " Корни: " + x2 + ";" + x1;
-            }
-            {
-                return "Количество решений: 2." + " Корни: " + x1 + ";" + x2;
-            }
+            double x1 = (-b + e) / (2 * a);
+            double x2 = (-b - e) / (2 * a);
+            return "Количество решений: 2." + " Корни: " + Math.min(x1, x2) + ";" + Math.max(x1, x2);
         }
-        {
-            return "Количество решений: 0.";
-        }
+        return "Количество решений: 0.";
     }
 }
