7d7db413e9ce50ac47e3f8fc779e9629368d794b
vlad <vob237@gmail.com>
2024-11-21 22:17:10 +0300

    fix Task3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,18 +29,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        if (year < 1) {
-            return false;
-        }
-
-        if (year % 400 == 0) {
-            return true;
-        }
-
-        if (year % 100 == 0) {
-            return false;
-        }
-
-        return year % 4 == 0;
+        return year >= 1 && ((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0));
     }
 }
