485040d0af384c5184310d7d35b0ab1456689e9d
Rita <margarita.mamatkazina@gmail.com>
2024-11-21 23:14:03 +0300

    Сложно

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -72,10 +72,6 @@ public class Task2 {
             floorNumber = (flatNumber - (entranceNumber - 1) * flatInEntrance) / 4 + 1;
         }
 
-        if (flatNumber % 4 == 1 || flatNumber % 4 == 2) {
-            boolean left = true;
-        }
-
         if (flatNumber > flatAmount) {
             return "Такой квартиры не существует";
 
