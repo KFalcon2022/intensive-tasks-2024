99c2f64d5ac05669231a3cea14033e1566f4c0f0
xBobrov <andy.beaver.vld@gmail.com>
2024-11-21 10:20:00 +1000

    task1 -v0.1 -21/11/24

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -14,15 +14,30 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
         int age = 0;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        int ageLastDigit = age % 10;
+        boolean isException = (age % 100 >= 11) && (age % 100 <= 14);
+        String ageInWords;
+
+        if (ageLastDigit == 1) {
+            ageInWords = " год";
+        } else if (ageLastDigit == 0 || ageLastDigit >= 5 ) {
+            ageInWords = " лет";
+        } else {
+            ageInWords = " года";
+        }
+
+        if (isException) {
+            ageInWords = " лет";
+        }
+
+        return "Вам " + age + ageInWords;
     }
 }
