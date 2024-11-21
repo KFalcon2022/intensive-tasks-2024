ed4c12d4a8f7d093531f95054f3095a1df3e6aa6
TOP1 <overseo4@gmail.com>
2024-11-21 09:48:25 +0500

    Внесены правки

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -34,12 +34,26 @@ public class Task4 {
     }
 
     static String solveEquation(double a, double b, double c) {
-        if (a == 0 && b == 0) return "Бесконечное множество решений.";
+        if (a == 0 && b == 0) {
+            if (c == 0) {
+                return "Бесконечное множество решений.";
+            } else {
+                return "Количество решений: 0.";
+            }
+        }
+        if (a == 0 && b != 0) {
+            return "Количество решений: 1. Корень: " + (int) (-c) / b;
+        }
+
         double discriminant = b * b - 4 * a * c;
         //System.out.println("d=" + String.valueOf(discriminant)); //для отладки
 
-        if (discriminant < 0) return "Количество решений: 0.";
-        else if (discriminant == 0) return "Количество решений: 1. Корень: " + (int) b / 2 * (-a);
+        if (discriminant < 0) {
+            return "Количество решений: 0.";
+        }
+        if (discriminant == 0) {
+            return "Количество решений: 1. Корень: " + (int) b / 2 * (-a);
+        }
         int x1 = (int) ((-b + Math.sqrt(discriminant)) / (2 * a));
         int x2 = (int) ((-b - Math.sqrt(discriminant)) / (2 * a));
 
