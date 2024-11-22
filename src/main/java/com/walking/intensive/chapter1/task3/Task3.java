99d042a5da7c8800eed5b6c122f7f6de293a48ee
Гепанов Антон <anton.hepanau@softclub.by>
2024-11-22 15:53:36 +0300

    Task3 commit (убрал второй if)

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -26,7 +26,7 @@ package com.walking.intensive.chapter1.task3;
 public class Task3 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int year = -2000;
+        int year = 2000;
         System.out.println(isLeap(year));
     }
 
@@ -34,10 +34,7 @@ public class Task3 {
         //Место для вашего кода
         if (year < 0) return false;
 
-        if ((year % 4 == 0 & year % 100 != 0) || (year % 400 == 0 & year % 100 == 0)) {
-            return true;
-        }
-        return false;
+        return (year % 4 == 0 & year % 100 != 0) || (year % 400 == 0 & year % 100 == 0);
     }
 }
 
