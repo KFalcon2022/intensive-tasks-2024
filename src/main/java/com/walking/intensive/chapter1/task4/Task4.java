ed5380e2082f4a835cbaffb344ae9fb4425263fe
Neverhood <just7500@gmail.com>
2024-11-22 21:48:34 +0800

    Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -1,32 +1,10 @@
 package com.walking.intensive.chapter1.task4;
 
-/**
- * ���� ���������:
- *
- * <p>ax? + bx + c = 0
- *
- * <p>���������� ����� solveEquation(), ������� ����������� ���������
- * ������������ - ������������ ����� a, b � c.
- *
- * <p>����� ������ ���������� � ���� ������ ���������� �������, � ����� ���� ������� � ��������� ���� �������:
- * <ul>
- * <li> "���������� �������: 2. �����: -4;4"
- * <li> "���������� �������: 1. ������: 0"
- * <li> "���������� �������: 0."
- * <li> "����������� ��������� �������."
- * </ul>
- *
- * <p>�������� ��������, ��� ���� ������ ��������� ��� - ��� ������ ������������� �� �����������.
- *
- * <p>P.S. ���������� ��������� �������� ���� ����� ������� �����, ���� ����� ������������.
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">���������� � ����������</a>
- */
 public class Task4 {
     public static void main(String[] args) {
-        double a = 0;
-        double b = -2;
-        double c = 2;
+        double a = -1;
+        double b = 3;
+        double c = 4;
 
         System.out.println(solveEquation(a, b, c));
     }
@@ -43,19 +21,20 @@ public class Task4 {
             return textMessage + "0.";
         }
 
-        if (a==0){
-            return textMessage+"1. "+(-c)/b;
+        if (a == 0) {
+            double root = -c / b + 0;
+            return textMessage + "1. Корень: " + String.format("%.0f", root); //Решаем как линейное уравнение с одним неизвестным
         }
 
         double discriminant = b * b - 4 * a * c; //Находим дискриминант
 
         if (discriminant > 0) {
-            double firstValue = (-b - Math.sqrt(discriminant)) / (2 * a); //Находим значение меньшего по значению корня
-            double secondValue = (-b + Math.sqrt(discriminant)) / (2 * a); // ... второго корня
-            textMessage = textMessage + "2. Корни: " + firstValue + "; " + secondValue;
+            double firstRoot = (-b - Math.sqrt(discriminant)) / (2 * a); //Находим значение корней
+            double secondRoot = (-b + Math.sqrt(discriminant)) / (2 * a);
+            textMessage = textMessage + "2. Корни: " + String.format("%.0f", Math.min(firstRoot, secondRoot)) + ";" + String.format("%.0f", Math.max(firstRoot, secondRoot));
         } else if (discriminant == 0) {
-            double firstValue = (-b - Math.sqrt(discriminant)) / (2 * a); //Значение корня при Д=0
-            textMessage = textMessage + "1. Корень: " + firstValue;
+            double root = (-b - Math.sqrt(discriminant) + 0) / (2 * a); //Значение корня при Д=0
+            textMessage = textMessage + "1. Корень: " + String.format("%.0f", root);
         } else {
             textMessage = textMessage + "0.";
         }
