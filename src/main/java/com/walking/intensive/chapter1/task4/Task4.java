13a4c4c47e03510bcb85dfc289fb308d5f3604d3
Yuri <zembekov.m@gmail.com>
2024-11-22 12:37:15 +0300

    task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,16 +26,53 @@ public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
-        double b = 0;
-        double c = 0;
+        double b = -2;
+        double c = 2;
 
         System.out.println(solveEquation(a, b, c));
+        System.out.println(0.0 * -1);
 
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        }
+        if (a == 0) {
+            if (b == 0) {
+                return "Количество решений: 0.";
+            }
+            double x = (-c / b);
+            if (x == -0.0) {
+                x = 0.0;
+            }
+            return "Количество решений: 1. Корень: " + x;
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        double discriminant = b * b - 4 * a * c;
+        if (discriminant < 0) {
+            return "Количество решений: 0.";
+        }
+        if (discriminant == 0) {
+            double x = (-b / (2 * a));
+            if (x == -0.0) {
+                x = 0.0;
+            }
+            return "Количество решений: 1. Корень: " + x;
+        }
+
+        double x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        double x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+        String templateResultForTwoX = "Количество решений: 2. Корни: ";
+        if (x1 == -0.0) {
+            x1 = 0.0;
+        }
+        if (x2 == -0.0) {
+            x2 = 0.0;
+        }
+        if (x1 < x2) {
+            return templateResultForTwoX + x1 + ";" + x2;
+        }
+        return templateResultForTwoX + x2 + ";" + x1;
     }
 }
