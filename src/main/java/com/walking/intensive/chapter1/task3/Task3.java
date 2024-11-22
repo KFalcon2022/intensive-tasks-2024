83e25eab0dd849ed5a0a7b0e8c3f6536107329d6
Rita <margarita.mamatkazina@gmail.com>
2024-11-22 19:27:38 +0300

    скобочки

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -37,14 +37,18 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        if (year % 400 == 0)
-            return true;
 
-        if (year % 100 == 0)
+        if (year % 400 == 0) {
+            return true;
+        }
+
+        if (year % 100 == 0) {
             return false;
+        }
 
-        if (year % 4 == 0)
+        if (year % 4 == 0) {
             return true;
+        }
 
         return false;
     }
