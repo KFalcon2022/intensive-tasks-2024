cda6d7062db3adc89a708162e0dff7d0cbe23f20
Evgen03010 <evgen@gmail.com>
2024-11-22 12:47:40 +0300

    Решение 1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,7 +15,7 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 7 ;
+        int age = 15 ;
 
         System.out.println(getAgeString(age));
     }
@@ -29,13 +29,13 @@ public class Task1 {
         if (age > 127 || age < 0) {
             return "Некорректный ввод";
 
-        } else if (age % 10 == 0 || age % 100 >= 11 && age % 100 <= 14) {
+        } else if (age % 100 >= 11 && age % 100 <= 14) {
             res = "лет";
 
         } else if (age % 10 >= 2 && age % 10 <= 4) {
             res = "года";
 
-        } else if (age % 10 >= 5 && age % 10 <= 9) {
+        } else if (age % 10 == 0 || age % 10 >= 5 && age % 10 <= 9) {
             res = "лет";
 
         } else if (age % 10 == 1) {
