75760de022e5e33491fc0eeda225e763d6b56898
Юлия <souinntn@gmail.com>
2024-11-23 11:14:19 +0500

    готово

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -14,29 +14,29 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 21;
+
+        int age = 29;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-         String let = "лет";
-         String god = "год";
-         String goda = "года";
-         String result  = "Некорректный ввод";
+        String let = "лет";
+        String god = "год";
+        String goda = "года";
+        String result = "Некорректный ввод";
 
-         if (age < 0 || age > 122){
-             return result;
-         } else if (age % 10 == 0 || (age % 10 >= 5 && age % 10 <= 9 )){
-             result = let;
-         } else if ((age >= 11 && age < 15) || ( age % 100 >= 11 && age % 100 <= 15)){
-             result = let;
-         } else if (age % 10 >= 2 && age % 10 <= 4){
-             result = goda;
-         } else if (age % 10 == 1 && age == 1){
-             result = god;
-         }
+        if (age < 0 || age > 122) {
+            return result;
+        } else if (age % 10 == 0 || (age % 10 >= 5 && age % 10 <= 9)) {
+            result = let;
+        } else if ((age >= 11 && age < 15) || (age % 100 >= 11 && age % 100 <= 15)) {
+            result = let;
+        } else if (age % 10 >= 2 && age % 10 <= 4) {
+            result = goda;
+        } else if (age >= 1) {
+            result = god;
+        }
 
         return ("Вам " + age + " " + result);
 
