d9ea510f673910901b28fedcd8dc082844887f92
Rita <margarita.mamatkazina@gmail.com>
2024-11-23 22:51:41 +0300

    что-то получилось

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -42,19 +42,18 @@ public class Task4 {
 
     static String solveEquation(double a, double b, double c) {
 
-        if(a == 0 && b == 0 && c == 0) {
+        if (a == 0 && b == 0 && c == 0) {
             return "Бесконечное множество решений.";
         }
 
-        if(a == 0 && b == 0) {
+        if (a == 0 && b == 0) {
             return "Количество решений: 0.";
         }
 
-        if(a == 0) {
+        if (a == 0) {
             double x = -c / b;
             return "Количество решений: 1. Корень: " + x;
         }
-
 
         double D = (b * b) - (4 * a * c);
 
@@ -70,10 +69,10 @@ public class Task4 {
         double d = (double) Math.sqrt(D);
         double x1 = (-b + d) / (2 * a);
         double x2 = (-b - d) / (2 * a);
-        if(x1 > x2) {
+        if (x1 > x2) {
             return "Количество решений: 2. Корни: " + x2 + ";" + x1;
         }
 
-       return "Количество решений: 2. Корни: " + x1 + ";" + x2;
+        return "Количество решений: 2. Корни: " + x1 + ";" + x2;
     }
 }
