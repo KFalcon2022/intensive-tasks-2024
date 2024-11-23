2e7bc218554814d5fbbdf6e53e982341587443ea
xBobrov <andy.beaver.vld@gmail.com>
2024-11-23 13:18:42 +1000

    task4 -v0.1 -23/11/24

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -34,8 +34,45 @@ public class Task4 {
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        }
+
+        if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        }
+
+        if (a == 0) {
+            return "Количество решений: 1. Корень: " + calculateLinearRoot(b, c);
+        }
+
+        StringBuilder builder = new StringBuilder("Количество решений: ");
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant > 0) {
+            builder.append("2. Корни: ");
+            double x1 = calculateSquareRoot(a, b, -Math.sqrt(discriminant));
+            double x2 = calculateSquareRoot(a, b, Math.sqrt(discriminant));
+            builder.append(x1 > x2 ? x2 + ";" + x1 : x1 + ";" + x2);
+        } else if (discriminant == 0) {
+            builder.append("1. Корень: ");
+            builder.append(calculateSquareRoot(a, b, discriminant));
+        } else {
+            builder.append("0.");
+        }
+
+        return builder.toString();
+    }
+
+    static double calculateSquareRoot(double a, double b, double d) {
+
+        return (-b + d) / 2 * a;
+    }
+
+    static double calculateLinearRoot(double b, double c) {
+
+        return -c / b;
     }
 }
