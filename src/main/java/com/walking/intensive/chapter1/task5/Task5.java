b19fca009d438e707b30cc7853bdd4811e09cbeb
Neverhood <just7500@gmail.com>
2024-11-23 17:05:21 +0800

    Task5

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -27,20 +27,18 @@ public class Task5 {
 
     static double getAreaByHeron(double a, double b, double c) {
 
-        if (!isDegenerate(a,b,c)) {
+        if (!isDegenerate(a, b, c)) {
             return -1;
         }
 
-        double semiPerimeter = (a + b + c) / 2; //Полупериметр
+        double semiPerimeter = (a + b + c) / 2;
 
         return Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c));
     }
 
     static double[] getHeights(double a, double b, double c) {
 
-        boolean nondegenerateTriangle = (a + b) > c && (a + c) > b && (b + c) > a; //Проверяем условие невырожденного треугольника
-
-        if (!nondegenerateTriangle) {
+        if (!isDegenerate(a, b, c)) {
             return new double[0];
         }
 
@@ -55,9 +53,7 @@ public class Task5 {
 
     static double[] getMedians(double a, double b, double c) {
 
-        boolean nondegenerateTriangle = (a + b) > c && (a + c) > b && (b + c) > a; //Проверяем условие невырожденного треугольника
-
-        if (!nondegenerateTriangle) {
+        if (!isDegenerate(a, b, c)) {
             return new double[0];
         }
 
@@ -66,22 +62,22 @@ public class Task5 {
         mediansMassive[1] = Math.sqrt(2 * a * a + 2 * c * c - b * b) / 2;
         mediansMassive[2] = Math.sqrt(2 * c * c + 2 * b * b - a * a) / 2;
         Arrays.sort(mediansMassive);
+
         return mediansMassive;
     }
 
     static double[] getBisectors(double a, double b, double c) {
 
-        boolean nondegenerateTriangle = (a + b) > c && (a + c) > b && (b + c) > a; //Проверяем условие невырожденного треугольника
-
-        if (!nondegenerateTriangle) {
+        if (!isDegenerate(a, b, c)) {
             return new double[0];
         }
 
-        double[] bisectorsMassive = new double[3];
         double[] segmentsMultiplication = new double[3];
         segmentsMultiplication[0] = ((a * c) / (a + b)) * ((b * c) / (a + b));
         segmentsMultiplication[1] = ((a * b) / (a + c)) * ((b * c) / (a + c));
         segmentsMultiplication[2] = ((a * c) / (b + c)) * ((a * b) / (b + c));
+
+        double[] bisectorsMassive = new double[3];
         bisectorsMassive[0] = Math.sqrt(a * b - segmentsMultiplication[0]);
         bisectorsMassive[1] = Math.sqrt(a * c - segmentsMultiplication[1]);
         bisectorsMassive[2] = Math.sqrt(b * c - segmentsMultiplication[2]);
@@ -92,25 +88,21 @@ public class Task5 {
 
     static double[] getAngles(double a, double b, double c) {
 
-        boolean nondegenerateTriangle = (a + b) > c && (a + c) > b && (b + c) > a; //Проверяем условие невырожденного треугольника
-
-        if (!nondegenerateTriangle) {
+        if (!isDegenerate(a, b, c)) {
             return new double[0];
         }
 
         double[] angelsMassive = new double[3];
-        angelsMassive[0] = Math.acos((b * b + c * c - a * a) / (2 * b * c)) * 180 / Math.PI; //Наименьший угол
+        angelsMassive[0] = Math.acos((b * b + c * c - a * a) / (2 * b * c)) * 180 / Math.PI;
         angelsMassive[1] = Math.acos((a * a + c * c - b * b) / (2 * a * c)) * 180 / Math.PI;
-        angelsMassive[2] = Math.acos((b * b + a * a - c * c) / (2 * b * a)) * 180 / Math.PI; //Наибольший угол
+        angelsMassive[2] = Math.acos((b * b + a * a - c * c) / (2 * b * a)) * 180 / Math.PI;
         Arrays.sort(angelsMassive);
         return angelsMassive;
     }
 
     static double getInscribedCircleRadius(double a, double b, double c) {
 
-        boolean nondegenerateTriangle = (a + b) > c && (a + c) > b && (b + c) > a; //Проверяем условие невырожденного треугольника
-
-        if (!nondegenerateTriangle) {
+        if (!isDegenerate(a, b, c)) {
             return -1;
         }
 
@@ -119,27 +111,23 @@ public class Task5 {
 
     static double getCircumradius(double a, double b, double c) {
 
-        boolean nondegenerateTriangle = (a + b) > c && (a + c) > b && (b + c) > a; //Проверяем условие невырожденного треугольника
-
-        if (!nondegenerateTriangle) {
+        if (!isDegenerate(a, b, c)) {
             return -1;
         }
 
-        double semiPerimeter = (a + b + c) / 2; //Полупериметр
+        double semiPerimeter = (a + b + c) / 2;
 
         return (a * b * c) / (4 * Math.sqrt(semiPerimeter * (semiPerimeter - a) * (semiPerimeter - b) * (semiPerimeter - c)));
     }
 
     static double getAreaAdvanced(double a, double b, double c) {
 
-        boolean nondegenerateTriangle = (a + b) > c && (a + c) > b && (b + c) > a; //Проверяем условие невырожденного треугольника
-
-        if (!nondegenerateTriangle) {
+        if (!isDegenerate(a, b, c)) {
             return -1;
         }
 
-        double angel = Math.acos((a * a + b * b - c * c) / (2 * a * b)); //Угол в радианах
-        double sine = Math.sqrt(1 - Math.pow(Math.cos(angel), 2)); //Синус угла через косинус
+        double angel = Math.acos((a * a + b * b - c * c) / (2 * a * b));
+        double sine = Math.sqrt(1 - Math.pow(Math.cos(angel), 2));
 
         return (a * b * sine) / 2;
     }
