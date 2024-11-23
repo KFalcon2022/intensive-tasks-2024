12f73c41f3a2ff69e2c122980c202c67cc6a9128
Юлия <souinntn@gmail.com>
2024-11-23 15:29:31 +0500

    исправлен код: убраны переменные убрано ограничение по верхнему возрасту объеденины условия по "лет" замена result на значение вместо переменных убрано else исправлен конечный return

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,30 +15,23 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 
-        int age = 29;
+        int age = 111;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        String let = "лет";
-        String god = "год";
-        String goda = "года";
-        String result = "Некорректный ввод";
 
-        if (age < 0 || age > 122) {
-            return result;
-        } else if (age % 10 == 0 || (age % 10 >= 5 && age % 10 <= 9)) {
-            result = let;
-        } else if ((age >= 11 && age < 15) || (age % 100 >= 11 && age % 100 <= 15)) {
-            result = let;
-        } else if (age % 10 >= 2 && age % 10 <= 4) {
-            result = goda;
-        } else if (age >= 1) {
-            result = god;
+        if (age < 0 ) {
+            return "Некорректный ввод";
+        } if (age % 10 == 0 || age % 10 >= 5 && age % 10 <= 9 || age >= 11 && age < 15 || age % 100 >= 11 && age % 100 <= 15) {
+            return ("Вам " + age + " лет");
+        } if (age % 10 >= 2 && age % 10 <= 4) {
+            return ("Вам " + age + " года");
+        } if (age >= 1) {
+            return ("Вам " + age + " год");
         }
 
-        return ("Вам " + age + " " + result);
-
+        return "result";
     }
 }
