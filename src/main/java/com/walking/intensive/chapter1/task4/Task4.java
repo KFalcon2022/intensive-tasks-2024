29e76db9dcdb2d87abdfc1273ad371e99edc90f4
Roman <ivashovroman96@mail.ru>
2024-11-24 15:45:36 +0400

    Task4 1. Убрал else после return. 2. Разделил блоки кода пробелами.

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -39,40 +39,45 @@ public class Task4 {
         if (a == 0 && b == 0 & c == 0) {
             return "Бесконечное множество решений.";
         }
+
         double x1;
+
         if (a == 0) {
             x1 = -c / b;
+
             if (Double.isNaN(x1) || Double.isInfinite(x1)) {
                 return "Количество решений: 0.";
-            } else {
-                return "Количество решений: 1. Корень: " + x1;
             }
+            return "Количество решений: 1. Корень: " + x1;
         }
+
         if (b == 0 && c == 0) {
             x1 = sqrt(0);
             return "Количество решений: 1. Корень: " + x1;
         }
+
         double d = b * b - 4 * a * c;
         double x2;
+
         if (d > 0) {
             x1 = (-b + sqrt(d)) / (2 * a);
             x2 = (-b - sqrt(d)) / (2 * a);
+
             if (x1 > x2) {
                 return "Количество решений: 2. Корни: " + x2 + ";" + x1;
-            } else {
-                return "Количество решений: 2. Корни: " + x1 + ";" + x2;
             }
+
+            return "Количество решений: 2. Корни: " + x1 + ";" + x2;
         } else if (d == 0) {
             x1 = -b / (2 * a);
+
             if (Double.isNaN(x1) || Double.isInfinite(x1)) {
                 return "Количество решений: 0.";
-            } else {
-                return "Количество решений: 1. Корень: " + x1;
             }
-        } else {
-            return "Количество решений: 0.";
+
+            return "Количество решений: 1. Корень: " + x1;
         }
 
+        return "Количество решений: 0.";
     }
-
 }
