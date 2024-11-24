c6987d60ed07a70117dc243a2ea78759a12efafe
RRAkerman <fajro1361@gmail.com>
2024-11-24 23:24:56 +0700

    Update Task1.java
    Tack 1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,14 +15,27 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
 
-        System.out.println(getAgeString(age));
+        final int AGE = 1;
+        System.out.println(getAgeString(AGE));
     }
 
-    static String getAgeString(int age) {
-//        Место для вашего кода
+    static String getAgeString(final int AGE) {
+        if (AGE >= 0) {
+            int divideAge = AGE;
+            boolean isException = (AGE % 100 < 10) || (AGE % 100 > 14);
 
-        return null; // Заглушка. При реализации - удалить
+            if (isException) {
+                divideAge = AGE % 10;
+            }
+
+            if (divideAge == 1) {
+                return "Вам " + AGE + " год";
+            } else if (divideAge > 1 && divideAge < 5) {
+                return "Вам " + AGE + " года";
+            }
+            return "Вам " + AGE + " лет";
+        }
+        return "Некорректный ввод";
     }
 }
