8a6cda9f8df5fd26ede85058f66493edaca2d64f
Vlad_KO <71587074+Allcash116@users.noreply.github.com>
2024-11-24 21:37:37 +0300

    Chapter 1, Task 5

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -23,9 +23,16 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getAreaByHeron(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return 0; // Заглушка. При реализации - удалить
+        double Area;
+        if (a < 1 | b < 1 | c < 1) {
+            return -1;
+        } else if (a > (b + c) | b > (a + c) | c > (a + b)) {
+            return -1;
+        }
+        double Perimetr = a + b + c;
+        double HalfPerimetr = Perimetr / 2;
+        Area = Math.sqrt(HalfPerimetr * (HalfPerimetr - a) * (HalfPerimetr - b) * (HalfPerimetr - c));
+        return Area;
     }
 
     /**
@@ -36,9 +43,37 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getHeights(double a, double b, double c) {
-        //        Место для вашего кода
+        double[] emptyArr = new double[0];
+        double[] arr = new double[3];
+        double Area = getAreaByHeron(a, b, c);
+        double tmp;
 
-        return null; // Заглушка. При реализации - удалить
+        if (a < 1 | b < 1 | c < 1) {
+            return emptyArr;
+        } else if (a > (b + c) | b > (a + c) | c > (a + b)) {
+            return emptyArr;
+        }
+
+        arr[0] = (2 * Area) / a;
+        arr[1] = (2 * Area) / b;
+        arr[2] = (2 * Area) / c;
+
+        if (arr[0] > arr[1]) {
+            tmp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = tmp;
+        }
+        if (arr[0] > arr[2]) {
+            tmp = arr[0];
+            arr[0] = arr[2];
+            arr[2] = tmp;
+        }
+        if (arr[1] > arr[2]) {
+            tmp = arr[1];
+            arr[1] = arr[2];
+            arr[2] = tmp;
+        }
+        return arr;
     }
 
     /**
@@ -49,9 +84,38 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getMedians(double a, double b, double c) {
-        //        Место для вашего кода
+        double[] emptyArr = new double[0];
+        double[] arr = new double[3];
+        double tmp;
 
-        return null; // Заглушка. При реализации - удалить
+
+        if (a < 1 | b < 1 | c < 1) {
+            return emptyArr;
+        } else if (a > (b + c) | b > (a + c) | c > (a + b)) {
+            return emptyArr;
+        }
+
+        arr[0] = Math.sqrt(2 * a * a + 2 * b * b - c * c) / 2;
+        arr[1] = Math.sqrt(2 * a * a + 2 * c * c - b * b) / 2;
+        arr[2] = Math.sqrt(2 * c * c + 2 * b * b - a * a) / 2;
+
+        if (arr[0] > arr[1]) {
+            tmp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = tmp;
+        }
+        if (arr[0] > arr[2]) {
+            tmp = arr[0];
+            arr[0] = arr[2];
+            arr[2] = tmp;
+        }
+        if (arr[1] > arr[2]) {
+            tmp = arr[1];
+            arr[1] = arr[2];
+            arr[2] = tmp;
+        }
+
+        return arr;
     }
 
     /**
@@ -62,9 +126,38 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getBisectors(double a, double b, double c) {
-        //        Место для вашего кода
+        double[] emptyArr = new double[0];
+        double[] arr = new double[3];
+        double tmp;
+        double Perimetr = a + b + c;
+        double HalfPerimetr = Perimetr / 2;
 
-        return null; // Заглушка. При реализации - удалить
+        if (a < 1 | b < 1 | c < 1) {
+            return emptyArr;
+        } else if (a > (b + c) | b > (a + c) | c > (a + b)) {
+            return emptyArr;
+        }
+
+        arr[0] = (2 * Math.sqrt(a * b * HalfPerimetr * (HalfPerimetr - c))) / (a + b);
+        arr[1] = (2 * Math.sqrt(a * c * HalfPerimetr * (HalfPerimetr - b))) / (a + c);
+        arr[2] = (2 * Math.sqrt(c * b * HalfPerimetr * (HalfPerimetr - a))) / (c + b);
+
+        if (arr[0] > arr[1]) {
+            tmp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = tmp;
+        }
+        if (arr[0] > arr[2]) {
+            tmp = arr[0];
+            arr[0] = arr[2];
+            arr[2] = tmp;
+        }
+        if (arr[1] > arr[2]) {
+            tmp = arr[1];
+            arr[1] = arr[2];
+            arr[2] = tmp;
+        }
+        return arr;
     }
 
     /**
@@ -75,9 +168,38 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getAngles(double a, double b, double c) {
-        //        Место для вашего кода
+        double[] emptyArr = new double[0];
+        double[] arr = new double[3];
+        double tmp;
 
-        return null; // Заглушка. При реализации - удалить
+
+        if (a < 1 | b < 1 | c < 1) {
+            return emptyArr;
+        } else if (a > (b + c) | b > (a + c) | c > (a + b)) {
+            return emptyArr;
+        }
+
+        arr[0] = Math.toDegrees(Math.acos(((b * b) + (c * c) - (a * a)) / (2 * b * c)));
+        arr[1] = Math.toDegrees(Math.acos(((a * a) + (c * c) - (b * b)) / (2 * a * c)));
+        arr[2] = Math.toDegrees(Math.acos(((b * b) + (a * a) - (c * c)) / (2 * b * a)));
+
+        if (arr[0] > arr[1]) {
+            tmp = arr[0];
+            arr[0] = arr[1];
+            arr[1] = tmp;
+        }
+        if (arr[0] > arr[2]) {
+            tmp = arr[0];
+            arr[0] = arr[2];
+            arr[2] = tmp;
+        }
+        if (arr[1] > arr[2]) {
+            tmp = arr[1];
+            arr[1] = arr[2];
+            arr[2] = tmp;
+        }
+        return arr;
+
     }
 
     /**
@@ -88,9 +210,17 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getInscribedCircleRadius(double a, double b, double c) {
-        //        Место для вашего кода
+        double Perimetr = a + b + c;
+        double HalfPerimetr = Perimetr / 2;
 
-        return 0; // Заглушка. При реализации - удалить
+        if (a < 1 | b < 1 | c < 1) {
+            return -1;
+        } else if (a > (b + c) | b > (a + c) | c > (a + b)) {
+            return -1;
+        }
+
+        return Math.sqrt(((HalfPerimetr - a) * (HalfPerimetr - b) * (HalfPerimetr - c)) / HalfPerimetr);
+
     }
 
     /**
@@ -101,9 +231,17 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getCircumradius(double a, double b, double c) {
-        //        Место для вашего кода
+        double Perimetr = a + b + c;
+        double HalfPerimetr = Perimetr / 2;
 
-        return 0; // Заглушка. При реализации - удалить
+        if (a < 1 | b < 1 | c < 1) {
+            return -1;
+        } else if (a > (b + c) | b > (a + c) | c > (a + b)) {
+            return -1;
+        }
+
+        return (a * b * c) / (4 * Math.sqrt(HalfPerimetr * (HalfPerimetr - a) * (HalfPerimetr - b) * (HalfPerimetr - c)));
+
     }
 
     /**
