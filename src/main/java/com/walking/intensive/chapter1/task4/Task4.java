60847e58594097f054dc6d91e2f0e70e3da70966
RRAkerman <fajro1361@gmail.com>
2024-11-24 23:31:14 +0700

    Update Task4.java
    Tack 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -25,17 +25,43 @@ package com.walking.intensive.chapter1.task4;
 public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        double a = 0;
-        double b = 0;
-        double c = 0;
+        double a = -1;
+        double b = 3;
+        double c = 4;
 
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        boolean isSqrt = (a != 0 && b != 0 && c != 0);
+        boolean isNonSqrtFirst = (a == 0 && b != 0 && c != 0);
+        boolean isNonSqrtSecond = ((a == 0 && b != 0 && c == 0) || (a != 0 && b == 0 && c == 0));
+        boolean isZero = (a == 0 && b == 0 && c == 0);
 
-        return null; // Заглушка. При реализации - удалить
+        if (isSqrt) {
+            final double discriminant = Math.pow(b, 2) - (4 * a * c);
+            double xFirst = 0;
+            double xSecond = 0;
+            if (discriminant > 0) {
+                xFirst = (-(b) + Math.sqrt(discriminant)) / 2 * a;
+                xSecond = (-(b) - Math.sqrt(discriminant)) / 2 * a;
+                if (xFirst < xSecond) {
+                    return "Количество решений: 2. Корни: " + xFirst + ";" + xSecond;
+                } else {
+                    return "Количество решений: 2. Корни: " + xSecond + ";" + xFirst;
+                }
+            } else if (discriminant == 0) {
+                xFirst = (-b) / 2 * a;
+                return "Количество решений: 1. Корень: " + xFirst;
+            }
+            return "Количество решений: 0.";
+        } else if (isNonSqrtFirst) {
+            return "Количество решений: 1. Корень: " + ((c / b) * (-1));
+        } else if (isNonSqrtSecond) {
+            return "Количество решений: 1. Корень: 0";
+        } else if (isZero) {
+            return "Бесконечное множество решений.";
+        }
+        return "Количество решений: 0.";
     }
 }
