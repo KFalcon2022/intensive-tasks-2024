e23149ca3133dc45d70c296d60d10b320f6da57a
ox000 <>
2024-11-24 15:38:24 +0300

    fix brackets in task 3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -35,6 +35,6 @@ public class Task3 {
 
     static boolean isLeap(int year) {
 
-        return ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) && year > 1582;
+        return (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) && year > 1582;
     }
 }
