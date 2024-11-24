bb7af139f6168b1fdbcc2739bf818e616f4234bc
Enhusikpro <axelkov@gmail.com>
2024-11-24 11:25:44 +0300

    пока непонятки с этажами

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -33,12 +33,41 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        int floorAmount = 10;
+        int entranceAmount = 2;
+        int flatNumber = 20;
+
+        System.out.println((getFlatLocation(floorAmount, entranceAmount, flatNumber)));
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        int entranceCount =  flatNumber / (floorAmount * 4);
+        if (entranceCount == 0) {
+            entranceCount = entranceCount + 1;
+        }
+        int floorCount = ((flatNumber - ((entranceCount + 1) * (floorAmount * 4))) / 4);
+
+        if (floorCount == 1) {
+            floorCount = floorCount + 1;
+        }
+
+        if (floorAmount <= 0 | entranceAmount <= 0 | flatNumber <= 0) {
+            return "Некорректные входные данные";
+
+        } else if (flatNumber > (floorAmount * entranceAmount * 4)) {
+            return "Такой квартиры не существует";
+        } else if ((flatNumber % 4) == 0) {
+            return flatNumber + " кв - " + entranceCount + " подъезд," + floorCount + " этаж, справа от лифта, вправо";
+
+        } else if ((flatNumber % 4) == 1) {
+            return flatNumber + " кв - " + entranceCount + " подъезд," + floorCount + " этаж, слева от лифта, влево";
+
+        } else if ((flatNumber % 4) == 2) {
+            return flatNumber + " кв - " + entranceCount + " подъезд," + floorCount + " этаж, слева от лифта, вправо";
+
+        } else {
+            return flatNumber + " кв - " + entranceCount + " подъезд," + floorCount + " этаж, справа от лифта, влево";
+        }
     }
 }
