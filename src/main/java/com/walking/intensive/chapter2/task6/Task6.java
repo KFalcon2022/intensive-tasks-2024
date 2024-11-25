cd678c45f7c551f2dbc245d94c44dbc2ccad32c2
sauza7 <sauza777@yandex.ru>
2024-11-25 13:18:37 +0300

    Task6_first_solution

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -8,7 +8,12 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+//        Для собственных проверок можете делать любые изменения в этом методе\
+        int m = 5;
+        int n = 6;
+        System.out.println("НОК = " + getLcm(m, n));
+        System.out.println("НОД = " + getGcd(m, n));
+        System.out.println("НОД2 = " + getGcdByEuclideanAlgorithm(m, n));
     }
 
     /**
@@ -19,8 +24,12 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getLcm(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (m < 1 || n < 1) {
+            return -1;
+        }
+
+        return (m * n) / getGcdByEuclideanAlgorithm(m, n);
     }
 
     /**
@@ -31,8 +40,22 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcd(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (m < 1 || n < 1) {
+            return -1;
+        }
+
+        if (m % n == 0 || n % m == 0) {
+            return Math.min(m, n);
+        }
+
+        int divisor = Math.min(m, n) / 2;
+
+        while (m % divisor != 0 || n % divisor != 0) {
+            divisor--;
+        }
+
+        return divisor;
     }
 
     /**
@@ -44,7 +67,17 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcdByEuclideanAlgorithm(int m, int n) {
-        // Ваш код
-        return 0;
+         /*
+         Для валидации n == 0 ? предложил бы ввести в параметры метода boolean isOriginalNumbers
+         и вызов метода из main c true
+         и тогда делал бы проверку: if (isOriginalNumbers && ( m < 1 || n < 1)) return -1;
+         а в рекурсии бы параметром шел бы false и до проверки n не дошли бы
+         */
+
+        if (m < 1 || n < 0) {
+            return -1;
+        }
+
+        return n == 0 ? m : (getGcdByEuclideanAlgorithm(n, m % n));
     }
 }
