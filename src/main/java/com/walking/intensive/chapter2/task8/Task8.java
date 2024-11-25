6ed2d041bcad8b5bb89dd3c4889d155a43af61bf
Neverhood <just7500@gmail.com>
2024-11-25 20:10:32 +0800

    Task8

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -1,30 +1,39 @@
 package com.walking.intensive.chapter2.task8;
 
-/**
- * Добираясь в школу на трамвае Вова проверяет, является ли купленный билет счастливым.
- * Если является, то нужно загадать желание и съесть билетик.
- *
- * <p>Билет содержит 6 цифр, комбинации подбираются случайным образом от 000000 до 999999.
- *
- * <p>Билет считается счастливым, если сумма первых трех цифр равна сумме последних трех цифр.
- *
- * <p>Пример: 123411 – счастливый (1 + 2 + 3 == 4 + 1 + 1).
- *
- * <p>Реализуйте метод getHappyTicketChance(), который будет возвращать вероятность
- * выпадения счастливого билета.
- *
- * <p>P.S. Вероятность – это отношение благоприятных исходов к числу всех исходов.
- * Вероятность не может принимать значение больше 1.
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
- */
+import java.util.Arrays;
+
 public class Task8 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        System.out.println(getHappyTicketChance());
     }
 
     static double getHappyTicketChance() {
-        // Ваш код
-        return 0.0;
+
+        int[] sumOfDigits = new int[28];    //Количество сумм (0-27) трех цифр числа от 000 до 999 -
+                                            //есть вероятность таких комбинаций на одну тысячу
+        int summ;                           //Сумма трех цифр числа
+        int currentDigit;                   //Разряд числа
+        double totalChance = 0;             //Вероятность совпадения двух сумм "счастливого" билета -
+
+        for (int i = 1; i < 1000; i++) {
+            currentDigit = i;
+            summ = 0;
+
+            while (currentDigit > 0) {
+                summ += currentDigit % 10;
+                currentDigit /= 10;
+            }
+
+            sumOfDigits[summ]++;
+        }
+
+        sumOfDigits[0] = 1;                 //Вероятность выпадения комбинации 000 = 1/1000
+
+        for (int i = 0; i < 28; i++) {
+            totalChance += Math.pow(sumOfDigits[i], 2);
+        }
+
+        return totalChance/1000000;
     }
 }
