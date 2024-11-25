f05419dd7b11452da7ab64c2efb94c62c2ea1d5c
yulsin <yulssnow@gmail.com>
2024-11-26 01:45:22 +0300

    task2.0

--- src/main/java/com/walking/intensive/chapter1/task2/Task2.java
+++ src/main/java/com/walking/intensive/chapter1/task2/Task2.java
@@ -33,12 +33,68 @@ package com.walking.intensive.chapter1.task2;
  */
 public class Task2 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        int floorAmount = 10;
+        int entranceAmount = -3;
+        int flatNumber = -41;
+
+        if (floorAmount < 1 || entranceAmount < 1 || flatNumber < 1) {
+            System.out.println("Некорректные входные данные");
+        } else if (floorAmount * entranceAmount * 4 < flatNumber) {
+            System.out.println("Такой квартиры не существует");
+        } else {
+            System.out.println(getFlatLocation(floorAmount, entranceAmount, flatNumber));
+        }
     }
 
     static String getFlatLocation(int floorAmount, int entranceAmount, int flatNumber) {
-        //        Место для вашего кода
+        int flatAmountInOneEntrance = floorAmount * 4;
+        int entranceNumber;
+        int floorNumber;
+        String str = null;
 
-        return null; // Заглушка. При реализации - удалить
+        if (flatNumber % flatAmountInOneEntrance == 0) {
+            entranceNumber = flatNumber / flatAmountInOneEntrance;
+        } else {
+            entranceNumber = 1 + flatNumber / flatAmountInOneEntrance;
+        }
+
+        if (flatAmountInOneEntrance <= flatNumber) {
+            if (flatNumber % flatAmountInOneEntrance == 0) {
+                floorNumber = floorAmount;
+            } else {
+                floorNumber = 1 + ((flatNumber / flatAmountInOneEntrance) / 2);
+            }
+        } else {
+            floorNumber = flatNumber / 4 + 1;
+        }
+
+        if (flatNumber < 11 || floorNumber / 3 <= 2 && (flatNumber / 20) == ((flatNumber + 9) / 20)) {
+            if (flatNumber % 10 == 1 || flatNumber % 10 == 5 || flatNumber % 10 == 9) {
+                str = "слева от лифта, влево";
+            }
+            if (flatNumber % 10 == 2 || flatNumber % 10 == 6 || flatNumber % 10 == 0) {
+                str = "слева от лифта, вправо";
+            }
+            if (flatNumber % 10 == 3 || flatNumber % 10 == 7) {
+                str = "справа от лифта, влево";
+            }
+            if (flatNumber % 10 == 4 || flatNumber % 10 == 8) {
+                str = "справа от лифта, вправо";
+            }
+        } else {
+            if (flatNumber % 10 == 1 || flatNumber % 10 == 5 || flatNumber % 10 == 9) {
+                str = "справа от лифта, влево";
+            }
+            if (flatNumber % 10 == 2 || flatNumber % 10 == 6 || flatNumber % 10 == 0) {
+                str = "справа от лифта, вправо";
+            }
+            if (flatNumber % 10 == 3 || flatNumber % 10 == 7) {
+                str = "слева от лифта, влево";
+            }
+            if (flatNumber % 10 == 4 || flatNumber % 10 == 8) {
+                str = "слева от лифта, вправо";
+            }
+        }
+        return "%d кв - %d подъезд, %d этаж, %s".formatted(flatNumber, entranceNumber, floorNumber, str);
     }
 }
