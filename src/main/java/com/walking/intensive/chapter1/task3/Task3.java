0330bb956c368e0f778bdc6eeac48b8f6d0089ef
Vlad_KO <71587074+Allcash116@users.noreply.github.com>
2024-11-26 21:31:20 +0300

    Chapter 1, Task 3

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -26,18 +26,10 @@ package com.walking.intensive.chapter1.task3;
 
 public class Task3 {
     public static void main(String[] args) {
+
     }
 
     static boolean isLeap(int year) {
-        if ( year % 400 == 0){
-            return true;
-        }
-        else if (year  % 100 == 0){
-            return false;
-        }
-        else if (year % 4 == 0){
-            return  true;
-        }
-        return false;
+        return ((year % 400 == 0 && year % 100 == 0) || ((year % 100 != 0 && year % 4 == 0)));
     }
 }
