f03c6ae8744ee141ff4cfc41a900e4113e64f6a6
Vlad_KO <71587074+Allcash116@users.noreply.github.com>
2024-11-27 01:41:09 +0300

    Task 4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -35,35 +35,41 @@ public class Task4 {
 
     static String solveEquation(double a, double b, double c) {
         double x1, x2;
-        double D = (b * b) - (4 * a * c);
+        double Discriminant = (b * b) - (4 * a * c);
+
         if (a == 0 & b == 0 & c == 0) {
             return "Бесконечное множество решений.";
         }
+
         if (a == 0 & b == 0) {
             return "Количество решений: 0.";
         }
+
         if (b == 0 & c == 0) {
             x1 = 0;
             return "Количество решений: 1. Корень: " + x1;
         }
+
         if (a == 0) {
             x1 = -(c / b);
             return "Количество решений: 1. Корень: " + x1;
         }
-        if (D < 0) {
+
+        if (Discriminant < 0) {
             return "Количество решений: 0.";
-        } else if (D == 0) {
+        }
+
+        if (Discriminant == 0) {
             x1 = -(b / (2 * a));
             return "Количество решений: 1. Корень: " + x1;
-        } else if (D > 0) {
-            x1 = (-b + Math.sqrt(D)) / (2 * a);
-            x2 = (-b - Math.sqrt(D)) / (2 * a);
-            if (x1 > x2) {
-                return "Количество решений: 2. Корни: " + x2 + ";" + x1;
-            } else {
-                return "Количество решений: 2. Корни: " + x1 + ";" + x2;
-            }
         }
-        return null;
+
+        x1 = (-b + Math.sqrt(Discriminant)) / (2 * a);
+        x2 = (-b - Math.sqrt(Discriminant)) / (2 * a);
+
+        if (x1 > x2) {
+            return "Количество решений: 2. Корни: " + x2 + ";" + x1;
+        }
+        return "Количество решений: 2. Корни: " + x1 + ";" + x2;
     }
 }
