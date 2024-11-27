7dc2cbc5683d2747125ec99ccd1d8efe29be7748
Evgencms <prcev888@rambler.ru>
2024-11-27 23:22:44 +0300

    Выполнено задание на расчёт НОД и НОК.

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -8,7 +8,15 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        System.out.println(getGcd(38, 18));
+        System.out.println();
+        System.out.println(getGcdByEuclideanAlgorithm(38, 18));
+    }
+
+    static boolean getValidation(int m, int n) {
+
+        return m <= 0 || n <= 0;
     }
 
     /**
@@ -19,8 +27,30 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getLcm(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (getValidation(m, n)) {
+            return -1;
+        }
+
+        int noc = 0, number1 = m, number2 = n;
+
+        if (number1 == number2) {
+            noc = number1;
+            return noc;
+        }
+
+        while (number1 != number2) {
+            while (number1 < number2) {
+                number1 += m;
+            }
+            while (number1 > number2) {
+                number2 += n;
+            }
+
+            noc = number1;
+        }
+
+        return noc;
     }
 
     /**
@@ -31,8 +61,29 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcd(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (getValidation(m, n)) {
+            return -1;
+        }
+
+        int nod = 0, number1 = 0, number2 = 0;
+
+        for (int i = 1; i <= m; i++) {
+            if (m % i == 0) {
+                number1 = i;
+            }
+            for (int j = 1; j <= n; j++) {
+                if (n % j == 0) {
+                    number2 = j;
+
+                    if (number1 == number2) {
+                        nod = number1;
+                    }
+                }
+            }
+        }
+
+        return nod;
     }
 
     /**
@@ -44,7 +95,15 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcdByEuclideanAlgorithm(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (m <= 0) {
+            return -1;
+        }
+
+        if (n == 0) {
+            return m;
+        }
+
+        return getGcdByEuclideanAlgorithm(n, m % n);
     }
 }
