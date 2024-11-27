fa8bc549fef39fbe0c68e39e79af095069bf4ad1
Anton <hellriser2007@gmail.com>
2024-11-27 17:15:05 +0200

    Task 7 initial commit

--- src/main/java/com/walking/intensive/chapter2/task7/Task7.java
+++ src/main/java/com/walking/intensive/chapter2/task7/Task7.java
@@ -28,10 +28,44 @@ package com.walking.intensive.chapter2.task7;
 public class Task7 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(getFriendlyPair(10000));
     }
 
     static int getFriendlyPair(int n) {
-        // Ваш код
-        return 0;
+        if (n < 1 || n > 1000000) {
+            return -1;
+        }
+
+        int maxSum = 0;
+        int greaterNumber = 0;
+
+        for (int i = 1; i < n; i++) {
+            int secondNumber = getDividers(i);
+
+            if (secondNumber < n && getDividers(secondNumber) == i && i != secondNumber) {
+                int currentSum = secondNumber + i;
+
+                if (currentSum > maxSum) {
+                    maxSum = currentSum;
+                    greaterNumber = Math.max(i, secondNumber);
+                }
+            }
+        }
+
+        return greaterNumber;
+    }
+
+    static int getDividers(int d) {
+        int sum = 0;
+        int limiter = d - 1;
+
+        for (int i = 1; i < limiter; i++) {
+
+            if (d % i == 0) {
+                sum += i;
+            }
+        }
+
+        return sum;
     }
 }
