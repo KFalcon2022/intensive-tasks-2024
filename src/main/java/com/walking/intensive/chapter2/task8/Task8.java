29193e327bb68e4c1023f50f13204ed4ad05d434
GepAnt <gepan@tut.by>
2024-11-28 16:57:57 +0300

    Task8 commit

--- src/main/java/com/walking/intensive/chapter2/task8/Task8.java
+++ src/main/java/com/walking/intensive/chapter2/task8/Task8.java
@@ -1,7 +1,5 @@
 package com.walking.intensive.chapter2.task8;
 
-import java.util.Arrays;
-
 /**
  * Добираясь в школу на трамвае Вова проверяет, является ли купленный билет счастливым.
  * Если является, то нужно загадать желание и съесть билетик.
@@ -28,39 +26,26 @@ public class Task8 {
 
     static double getHappyTicketChance() {
         // Ваш код
-        int maxCifra = 9;   // кол-во исходов основание, степень всегда 3
-        int s = (int) Math.pow(10.0, 3.0);
-        int[] sums = new int[s];
-        int index = 0;
-        for (int n = -1; n < maxCifra; n++) {
-            int[] h1 = new int[3];
-            h1[0] = n + 1;
-            for (int m = -1; m < maxCifra; m++) {
-                h1[1] = m + 1;
-                for (int k = -1; k < maxCifra; k++) {
-                    h1[2] = k + 1;
-                    int sum = h1[0] + h1[1] + h1[2];
-                    sums[index] = sum;
-                    index++;
-                  //  System.out.println(Arrays.toString(h1) + " " + sum);
-                }
-            }
-        }
-        int count = 0;
-        for (int i = 0; i < sums.length; i++) {
-            boolean isDuplicate = false;
-            for (int j = 1; j < sums.length; j++) {
-                if (sums[i] == sums[j]) {
-                    isDuplicate = true;
-                    break;
-                }
-            }
-            if (isDuplicate) {
-                count++;
-            }
-        }
-        double p=count/1000000.0;
-        return p; //return 0.0;
-    }
+        int maxCifra = 9;
+        int[] sums = new int[28];
 
+        for (int h1 = 0; h1 <= maxCifra; h1++) {
+            for (int h2 = 0; h2 <= maxCifra; h2++) {
+                for (int h3 = 0; h3 <= maxCifra; h3++) {
+
+                    int sum = h1 + h2 + h3;
+                    sums[sum]++;
+                }
+            }
+        }
+
+        int winningcount = 0;
+        for (int count : sums) {
+            winningcount = winningcount + count * count;
+        }
+
+        double allcount = 1000000.0;
+        double p = winningcount / allcount;
+        return p;
+    }
 }
