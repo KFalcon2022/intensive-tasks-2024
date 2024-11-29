43aeaf7cbe22dd1394175493d1edef7c73f7c47f
egor.djun <xedshotg@gmail.com>
2024-11-29 12:26:55 +0700

    task 15

--- src/main/java/com/walking/intensive/chapter3/task15/Task15.java
+++ src/main/java/com/walking/intensive/chapter3/task15/Task15.java
@@ -40,11 +40,97 @@ package com.walking.intensive.chapter3.task15;
  */
 public class Task15 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        //          j =  0  1  2  3
+        int[][] city = {{3, 0, 8, 4},  // i = 0
+                {2, 4, 5, 7},  // i = 1
+                {9, 2, 6, 3},  // i = 2
+                {0, 3, 1, 0}}; // i = 3
+
+        System.out.println(getMaxFloors(city));
     }
 
     static int getMaxFloors(int[][] city) {
-        // Ваш код
-        return 0;
+        if (!isValidate(city)) {
+            return -1;
+        }
+
+        int floors = 0;
+
+        for (int i = 0; i < city.length; i++) {
+
+            // находим максимальную высоту этожа на i - горизонтали
+            int maxHorizon = getMaxHorizonFloor(city, i);
+            int maxVertical;
+
+            for (int j = 0; j < city[i].length; j++) {
+
+                // находим максимальную высоту этажа на j - вертикали
+                maxVertical = getMaxVerticalFloor(city, j);
+
+                // находим для текущего этажа максимальную возможную пристройку
+                floors += getMaxFloor(maxHorizon, maxVertical, city[i][j]);
+            }
+
+        }
+
+        return floors;
+    }
+
+    private static int getMaxFloor(int maxHorizon, int maxVertical, int floor) {
+        return Math.min(maxHorizon, maxVertical) - floor;
+    }
+
+    private static int getMaxVerticalFloor(int[][] city, int index) {
+        int maxFloor = 0;
+
+        for (int[] ints : city) {
+            for (int j = index; j < index + 1; j++) {
+                if (ints[j] >= maxFloor) {
+                    maxFloor = ints[j];
+                }
+            }
+        }
+
+        return maxFloor;
+    }
+
+    private static int getMaxHorizonFloor(int[][] city, int index) {
+        int maxFloor = 0;
+
+        for (int j = 0; j < city[index].length; j++) {
+            if (city[index][j] >= maxFloor) {
+                maxFloor = city[index][j];
+            }
+        }
+
+        return maxFloor;
+    }
+
+    private static boolean isValidate(int[][] array) {
+        if (array.length < 1) {
+            return false;
+        }
+
+        for (int[] ints : array) {
+            if (ints.length < 1) {
+                return false;
+            }
+        }
+
+        int len = array[0].length;
+
+        for (int[] ints : array) {
+            if (ints.length > len || ints.length < len) {
+                return false;
+            }
+        }
+
+        for (int[] ints : array) {
+            for (int anInt : ints) {
+                if (anInt < 0) return false;
+            }
+        }
+
+        return true;
     }
 }
