a13441551e4e5347d51873d665d4acfea3b381d9
Anton <goptarevanton@gmail.com>
2024-11-30 22:05:53 +0300

    task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -24,18 +24,46 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
         double a = 0;
-        double b = 0;
-        double c = 0;
+        double b = -2;
+        double c = 2;
 
         System.out.println(solveEquation(a, b, c));
 
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
 
-        return null; // Заглушка. При реализации - удалить
+        double x1;
+        double x2;
+
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        }
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0 || a == 0 && b == 0) {
+            return "Количество решений: 0.";
+
+        } else if (discriminant == 0 || a == 0) {
+
+            x1 = a == 0 ? (-c / b) : (-b / 2 * a);
+            x1 = x1 == -0 ? 0 : x1;
+
+            return "Количество решений: 1. Корень: " + x1;
+        }
+
+        x1 = ((-b - Math.sqrt(discriminant)) / 2 * a);
+        x2 = ((-b + Math.sqrt(discriminant)) / 2 * a);
+
+        if (x1 > x2) {
+            double temp = x1;
+            x1 = x2;
+            x2 = temp;
+        }
+
+        return "Количество решений: 2. Корни: %.1f;%.1f".formatted(x1, x2);
     }
 }
