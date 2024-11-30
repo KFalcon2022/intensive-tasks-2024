214a89679b0a25f4788b30efcfcffb66c02de8c5
Neverhood <just7500@gmail.com>
2024-11-30 09:44:47 +0800

    Task10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -1,16 +1,5 @@
 package com.walking.intensive.chapter2.task10;
 
-import java.lang.reflect.Array;
-
-/**
- * Реализуйте метод isPalindrome(), который проверяет, является ли строка палиндромом.
- *
- * <p>Метод должен игнорировать пунктуацию, пробелы и регистр.
- *
- * <p>P.S. Мой любимый палиндром: Муза! Ранясь шилом опыта, ты помолишься на разум.
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
- */
 public class Task10 {
     public static void main(String[] args) {
 
@@ -19,20 +8,19 @@ public class Task10 {
 
     static boolean isPalindrome(String inputString) {
 
-        if (inputString == null || inputString.length() <2){
+        if (inputString == null || inputString.length() < 2) {
 
             return false;
         }
 
-        inputString = inputString.toUpperCase();
-        inputString = inputString.replaceAll("[^\\p{L}]", "");
-        char[] cleanCharacters = inputString.toCharArray();
+        String modifiedInput = inputString.toUpperCase().replaceAll("[^\\p{L}]", "");
+        char[] cleanCharacters = modifiedInput.toCharArray();
         String outputString = "";
 
-        for (int i = cleanCharacters.length - 1; i >=0 ; i--) {
+        for (int i = cleanCharacters.length - 1; i >= 0; i--) {
             outputString += cleanCharacters[i];
         }
 
-        return outputString.equals(inputString);
+        return outputString.equals(modifiedInput);
     }
 }
