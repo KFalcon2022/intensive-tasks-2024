735619b1b63ae2862dbf866190ebb0633530a387
alexa <alexa.markova@outlook.com>
2024-11-30 12:29:07 +0300

    commit task6_v1

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -19,8 +19,10 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getLcm(int m, int n) {
-        // Ваш код
-        return 0;
+        if (n <= 0 || m <= 0) {
+            return -1;
+        }
+        return m * n / getGcd(m, n);
     }
 
     /**
@@ -31,8 +33,21 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcd(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (n <= 0 || m <= 0) {
+            return -1;
+        }
+
+        while (m != n) {
+
+            if (m > n) {
+                m = m - n;
+            } else {
+                n = n - m;
+            }
+        }
+        return n;
+
     }
 
     /**
@@ -44,7 +59,17 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcdByEuclideanAlgorithm(int m, int n) {
-        // Ваш код
-        return 0;
+        if (n <= 0 || m <= 0) {
+            return -1;
+        }
+        return getGcdWithRecursive(Math.max(m, n), Math.min(m, n));
+    }
+
+    static int getGcdWithRecursive(int m, int n) {
+        int r = m % n;
+        if (r == 0) {
+            return n;
+        }
+        return getGcdWithRecursive(Math.max(n, r), Math.min(n, r));
     }
 }
