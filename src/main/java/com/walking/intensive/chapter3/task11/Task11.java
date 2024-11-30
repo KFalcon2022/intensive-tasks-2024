e66092ffda66c03e7d6cdf6ece71935e30753e14
Neverhood <just7500@gmail.com>
2024-11-30 16:12:01 +0800

    Task11

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -1,64 +1,18 @@
 package com.walking.intensive.chapter3.task11;
 
-/**
- * Реализуйте метод getOddSubArraysElementsSum() с учетом условий ниже.
- *
- * <p>Входной параметр метода - массив положительных целых чисел.
- *
- * <p>Возвращаемое значение - сумма элементов всех возможных подмассивов нечетной длины.
- * Подмассивом считается непрерывная последовательность элементов массива.
- *
- * <p>Например:
- *
- * <p>Входные данные: array = [1,4,2,5,3]
- *
- * <p>Подмассивы и суммы их элементов:
- *
- * <ul>
- * <li>[1] = 1
- * <li>[4] = 4
- * <li>[2] = 2
- * <li>[5] = 5
- * <li>[3] = 3
- * <li>[1,4,2] = 7
- * <li>[4,2,5] = 11
- * <li>[2,5,3] = 10
- * <li>[1,4,2,5,3] = 15
- * </ul>
- *
- * <p>Итоговая сумма: 1 + 4 + 2 + 5 + 3 + 7 + 11 + 10 + 15 = 58
- *
- * <p>Возвращаемое значение: 58
- *
- * <p>Для пустого массива должен возвращаться 0.
- *
- * <p>При наличии любых некорректных данных - например, неположительных элементов входящего массива, метод должен
- * возвращать -1.
- *
- * <p>P.S. Решение не должно использовать сортировки, коллекции, Stream API и иной материал, выходящий за рамки
- * пройденного курса.
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
- */
 public class Task11 {
     public static void main(String[] args) {
 
-//        int[] array = new int[5];
-//
-//        for (int i = 0; i < array.length; i++) {
-//            array[i] = i + 1;
-//        }
-
-//        System.out.println(getOddSubArraysElementsSum(array));
     }
 
     static int getOddSubArraysElementsSum(int[] array) {
 
-        for (int i = 0; i < array.length; i++) {
-            if (array[i]<0) {
+        for (int value : array) {
+            if (value < 0) {
                 return -1;
             }
         }
+
         int subArrayLength = array.length - (array.length + 1) % 2;
         int sum = 0;
 
@@ -71,8 +25,8 @@ public class Task11 {
             }
 
             subArrayLength -= 2;
-        } while (subArrayLength > 0);
 
+        } while (subArrayLength > 0);
 
         return sum;
     }
