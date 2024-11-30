ab13eee90af0f6f014cba52ad314195c0b755101
Kirill <kiry.klimov.231@gmail.com>
2024-11-30 23:37:35 +0300

    поправил замечания

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,21 +15,24 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 1;
+        int age = 13;
 
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-        if (age < 0) {
+
+        if (age < 0 || age >= 128) {
             return "Некорректный ввод";
         }
-        if ((age % 10 == 1) && (age / 10 % 10 != 1)) {
-            return ("Вам" + " " + age + " " + "год");
+
+        if (age % 10 == 1 && (age / 10 % 10 != 1)) {
+            return ("Вам " + age + " год");
         }
-        if (((age % 10 == 2) || (age % 10 == 3) || age % 10 == 4) && (age / 10 % 10 != 1)) {
+
+        if ((age % 10 == 2 || age % 10 == 3 || age % 10 == 4) && (age / 10 % 10 != 1)) {
             return ("Вам " + age + " года");
-        } else
-            return ("Вам " + age + " лет");
+        }
+        return ("Вам " + age + " лет");
     }
 }
