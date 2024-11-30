f3d033e63e30240539b6b4f32daeed884c753cec
Kirill <kiry.klimov.231@gmail.com>
2024-11-30 23:19:13 +0300

    changed with no "if"

--- src/main/java/com/walking/intensive/chapter1/task3/Task3.java
+++ src/main/java/com/walking/intensive/chapter1/task3/Task3.java
@@ -29,12 +29,6 @@ public class Task3 {
     }
 
     static boolean isLeap(int year) {
-        if (year <= 0) {
-            return false;
-        }
-        if ((year % 4 == 0 && year % 100 != 0) || year % 400 == 0) {
-            return true;
-        }
-        return false;
+        return (year > 0 && (year % 400 == 0 || (year % 100 != 0 && year % 4 == 0)));
     }
 }
