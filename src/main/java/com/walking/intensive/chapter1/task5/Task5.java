dc270dc97a9073d2d5a7d834e2e55f2f8e20e1b4
Alex <muav1902@gmail.ru>
2024-11-30 22:54:09 +0300

    Убрал неиспользуемые импорты. Переделал сортировку. Использовал метод подсчета площади. Вынес проверку в отдельный метод. Изменил имена переменных.

--- src/main/java/com/walking/intensive/chapter1/task5/Task5.java
+++ src/main/java/com/walking/intensive/chapter1/task5/Task5.java
@@ -1,6 +1,5 @@
 package com.walking.intensive.chapter1.task5;
 
-import java.lang.reflect.Array;
 import java.util.Arrays;
 
 /**
@@ -13,10 +12,9 @@ import java.util.Arrays;
  */
 public class Task5 {
     public static void main(String[] args) {
-        double a = -6;
-        double b = 8;
-        double c = 5;
-
+        double a = 17;
+        double b = 1;
+        double c = 1;
 
         System.out.println(getAreaByHeron(a, b, c));
 
@@ -29,8 +27,8 @@ public class Task5 {
         double[] toBisect = getBisectors(a, b, c);
         System.out.println(Arrays.toString(toBisect));
 
-        double[] toCalculateTheAngles = getAngles(a, b, c);
-        System.out.println(Arrays.toString(toCalculateTheAngles));
+        double[] angles = getAngles(a, b, c);
+        System.out.println(Arrays.toString(angles));
 
         System.out.println(getInscribedCircleRadius(a, b, c));
 
@@ -46,12 +44,20 @@ public class Task5 {
      *
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
-    static double getAreaByHeron(double a, double b, double c) {
+
+    static double check(double a, double b, double c) {
         if (a + b < c || b + c < a || a + c < b || a <= 0 || b <= 0 || c <= 0) {
             return -1;
         }
-        double p = (a + b + c) / 2;
+        return 0;
+    }
 
+
+    static double getAreaByHeron(double a, double b, double c) {
+        if (check(a, b, c) == -1) {
+            return -1;
+        }
+        double p = (a + b + c) / 2;
         return Math.sqrt((p * (p - a) * (p - b) * (p - c)));
     }
 
@@ -63,47 +69,15 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getHeights(double a, double b, double c) {
-        if (a + b < c || b + c < a || a + c < b || a <= 0 || b <= 0 || c <= 0) {
+        if (check(a, b, c) == -1) {
             return new double[]{};
         }
-        double p = (a + b + c) / 2;
-        double s = Math.sqrt((p * (p - a) * (p - b) * (p - c)));
-
-        double h0 = (2 * s) / a;
-        double h1 = (2 * s) / b;
-        double h2 = (2 * s) / c;
+        double h0 = (2 * getAreaByHeron(a, b, c)) / a;
+        double h1 = (2 * getAreaByHeron(a, b, c)) / b;
+        double h2 = (2 * getAreaByHeron(a, b, c)) / c;
 
         double[] heightsOfTriangle = new double[]{h0, h1, h2};
-        if (h0 > h1 && h1 > h2) {
-            heightsOfTriangle[0] = h2;
-            heightsOfTriangle[1] = h1;
-            heightsOfTriangle[2] = h0;
-        }
-        if (h0 > h2 && h2 > h1) {
-            heightsOfTriangle[0] = h1;
-            heightsOfTriangle[1] = h2;
-            heightsOfTriangle[2] = h0;
-        }
-        if (h1 > h0 && h0 > h2) {
-            heightsOfTriangle[0] = h2;
-            heightsOfTriangle[1] = h0;
-            heightsOfTriangle[2] = h1;
-        }
-        if (h1 > h2 && h2 > h0) {
-            heightsOfTriangle[0] = h0;
-            heightsOfTriangle[1] = h2;
-            heightsOfTriangle[2] = h1;
-        }
-        if (h2 > h0 && h0 > h1) {
-            heightsOfTriangle[0] = h1;
-            heightsOfTriangle[1] = h0;
-            heightsOfTriangle[2] = h2;
-        }
-        if (h2 > h1 && h1 > h0) {
-            heightsOfTriangle[0] = h0;
-            heightsOfTriangle[1] = h1;
-            heightsOfTriangle[2] = h1;
-        }
+        Arrays.sort(heightsOfTriangle);
         return heightsOfTriangle;
     }
 
@@ -115,45 +89,16 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getMedians(double a, double b, double c) {
-        if (a + b < c || b + c < a || a + c < b || a <= 0 || b <= 0 || c <= 0) {
+        if (check(a, b, c) == -1) {
             return new double[]{};
         }
         double medianA = 0.5 * (Math.sqrt((2 * Math.pow(b, 2)) + (2 * Math.pow(c, 2)) - Math.pow(a, 2)));
         double medianB = 0.5 * (Math.sqrt((2 * Math.pow(a, 2)) + (2 * Math.pow(c, 2)) - Math.pow(b, 2)));
         double medianC = 0.5 * (Math.sqrt((2 * Math.pow(a, 2)) + (2 * Math.pow(b, 2)) - Math.pow(c, 2)));
 
-        double[] medianOfTriangle = new double[]{medianA, medianB, medianC};
-        if (medianA > medianB && medianB > medianC) {
-            medianOfTriangle[0] = medianC;
-            medianOfTriangle[1] = medianB;
-            medianOfTriangle[2] = medianA;
-        }
-        if (medianA > medianC && medianC > medianB) {
-            medianOfTriangle[0] = medianB;
-            medianOfTriangle[1] = medianC;
-            medianOfTriangle[2] = medianA;
-        }
-        if (medianB > medianC && medianC > medianA) {
-            medianOfTriangle[0] = medianA;
-            medianOfTriangle[1] = medianC;
-            medianOfTriangle[2] = medianB;
-        }
-        if (medianB > medianA && medianA > medianC) {
-            medianOfTriangle[0] = medianC;
-            medianOfTriangle[1] = medianA;
-            medianOfTriangle[2] = medianB;
-        }
-        if (medianC > medianA && medianA > medianB) {
-            medianOfTriangle[0] = medianB;
-            medianOfTriangle[1] = medianA;
-            medianOfTriangle[2] = medianC;
-        }
-        if (medianC > medianB && medianB > medianA) {
-            medianOfTriangle[0] = medianA;
-            medianOfTriangle[1] = medianB;
-            medianOfTriangle[2] = medianC;
-        }
-        return medianOfTriangle;
+        double[] mediansOfTriangle = new double[]{medianA, medianB, medianC};
+        Arrays.sort(mediansOfTriangle);
+        return mediansOfTriangle;
     }
 
     /**
@@ -164,7 +109,7 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getBisectors(double a, double b, double c) {
-        if (a + b < c || b + c < a || a + c < b || a <= 0 || b <= 0 || c <= 0) {
+        if (check(a, b, c) == -1) {
             return new double[]{};
         }
         double cosA = ((b * b) + (c * c) - (a * a)) / (2 * b * c);
@@ -183,36 +128,7 @@ public class Task5 {
         double dc = ((2 * a * b) / (a + b)) * cosCDividedBy2;
 
         double[] toBisect = new double[]{da, db, dc};
-        if (da > db && db > dc) {
-            toBisect[0] = dc;
-            toBisect[1] = db;
-            toBisect[2] = da;
-        }
-        if (da > dc && dc > db) {
-            toBisect[0] = db;
-            toBisect[1] = dc;
-            toBisect[2] = da;
-        }
-        if (db > da && da > dc) {
-            toBisect[0] = dc;
-            toBisect[1] = da;
-            toBisect[2] = db;
-        }
-        if (db > dc && dc > da) {
-            toBisect[0] = da;
-            toBisect[1] = dc;
-            toBisect[2] = db;
-        }
-        if (dc > da && da > db) {
-            toBisect[0] = db;
-            toBisect[1] = da;
-            toBisect[2] = dc;
-        }
-        if (dc > db && db > da) {
-            toBisect[0] = da;
-            toBisect[1] = db;
-            toBisect[2] = dc;
-        }
+        Arrays.sort(toBisect);
         return toBisect;
     }
 
@@ -224,7 +140,7 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать пустой массив нулевой длины.
      */
     static double[] getAngles(double a, double b, double c) {
-        if (a + b < c || b + c < a || a + c < b || a <= 0 || b <= 0 || c <= 0) {
+        if (check(a, b, c) == -1) {
             return new double[]{};
         }
         double cosA = ((b * b) + (c * c) - (a * a)) / (2 * b * c);
@@ -239,39 +155,10 @@ public class Task5 {
         double angleBToDegree = Math.toDegrees(angleB);
         double angleCToDegree = Math.toDegrees(angleC);
 
-        double[] toCalculateTheAngles = new double[]{angleAToDegree, angleBToDegree, angleCToDegree};
+        double[] angles = new double[]{angleAToDegree, angleBToDegree, angleCToDegree};
 
-        if (angleA > angleB && angleB > angleC) {
-            toCalculateTheAngles[0] = angleCToDegree;
-            toCalculateTheAngles[1] = angleBToDegree;
-            toCalculateTheAngles[2] = angleAToDegree;
-        }
-        if (angleA > angleC && angleC > angleB) {
-            toCalculateTheAngles[0] = angleBToDegree;
-            toCalculateTheAngles[1] = angleCToDegree;
-            toCalculateTheAngles[2] = angleAToDegree;
-        }
-        if (angleB > angleA && angleA > angleC) {
-            toCalculateTheAngles[0] = angleCToDegree;
-            toCalculateTheAngles[1] = angleAToDegree;
-            toCalculateTheAngles[2] = angleBToDegree;
-        }
-        if (angleB > angleC && angleC > angleA) {
-            toCalculateTheAngles[0] = angleAToDegree;
-            toCalculateTheAngles[1] = angleCToDegree;
-            toCalculateTheAngles[2] = angleBToDegree;
-        }
-        if (angleC > angleB && angleB > angleA) {
-            toCalculateTheAngles[0] = angleAToDegree;
-            toCalculateTheAngles[1] = angleBToDegree;
-            toCalculateTheAngles[2] = angleCToDegree;
-        }
-        if (angleC > angleA && angleA > angleB) {
-            toCalculateTheAngles[0] = angleBToDegree;
-            toCalculateTheAngles[1] = angleAToDegree;
-            toCalculateTheAngles[2] = angleCToDegree;
-        }
-        return toCalculateTheAngles;
+        Arrays.sort(angles);
+        return angles;
     }
 
     /**
@@ -282,11 +169,10 @@ public class Task5 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static double getInscribedCircleRadius(double a, double b, double c) {
-        if (a + b < c || b + c < a || a + c < b || a <= 0 || b <= 0 || c <= 0) {
+        if (check(a, b, c) == -1) {
             return -1;
         }
         double p = (a + b + c) / 2;
-
         return getAreaByHeron(a, b, c) / p;
     }
 
@@ -299,10 +185,9 @@ public class Task5 {
      */
 
     static double getCircumradius(double a, double b, double c) {
-        if (a + b < c || b + c < a || a + c < b || a <= 0 || b <= 0 || c <= 0) {
+        if (check(a, b, c) == -1) {
             return -1;
         }
-
         return (a * b * c) / (4 * getAreaByHeron(a, b, c));
     }
 
