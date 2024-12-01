12089035289a57ac3f50f603c5b5b91827aed57c
Lizasha <violetcactusonline@gmail.com>
2024-12-01 01:29:47 +0100

    task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -26,16 +26,51 @@ public class Task4 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
-        double b = 0;
-        double c = 0;
+        double b = 2;
+        double c = -2;
 
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        double D;
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        } else if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        } else if (a == 0) {
+            double root = (-c / b);
+            if (root == -0.0) {
+                return "Количество решений: 1. Корень: " + 0.0;
+            } else {
+                return "Количество решений: 1. Корень: " + root;
+            }
+        }
+
+        double firstRoot;
+        double secondRoot;
+
+        // find D
+        D = b * b - 4 * a * c;
+
+        // find roots
+        if (D < 0) {
+            return "Количество решений: 0.";
+        } else if (D == 0) {
+            firstRoot = (-b - Math.sqrt(D)) / 2 * a;
+            firstRoot = firstRoot == 0 ? 0.0 : firstRoot;
+
+            return "Количество решений: 1. " + "Корень: " + firstRoot;
+        } else
+            firstRoot = (-b - Math.sqrt(D)) / 2 * a;
+            secondRoot = (-b + Math.sqrt(D)) / 2 * a;
+
+            String roots;
+            if (firstRoot < secondRoot) {
+                roots = firstRoot + ";" + secondRoot;
+            } else roots = secondRoot + ";" + firstRoot;
+
+            return "Количество решений: 2. " + "Корни: " + roots;
     }
 }
