e74d5afe36260701b1ff75018598797a091237c0
Neverhood <just7500@gmail.com>
2024-12-01 14:36:19 +0800

    Task13

--- src/main/java/com/walking/intensive/chapter3/task13/Task13.java
+++ src/main/java/com/walking/intensive/chapter3/task13/Task13.java
@@ -1,59 +1,63 @@
 package com.walking.intensive.chapter3.task13;
 
-/**
- * Ваша задача - с помощью лейки полить все растения в саду.
- * Всего в саду N растений. Они расположены в ряд и слева направо помечены
- * от 0 до N-1. Таким образом i-е растение расположено в точке x = i.
- *
- * <p>В точке x = -1 есть река, в которой вы можете наполнить свою лейку.
- *
- * <p>Каждому растению требуется определенное количество воды.
- * Полив растений производится следующим образом:
- *
- * <ol>
- * <li>Растения поливаются в порядке слева направо;
- * <li>Если после полива текущего растения, в лейке недостаточно воды, чтобы ПОЛНОСТЬЮ полить следующее растение,
- *      вы должны вернуться к реке, чтобы полностью наполнить лейку;
- * <li>Вы не можете наполнять лейку раньше времени. Единственный путь к реке - это выполнение пункта 2.
- * </ol>
- *
- * <p>Изначально вы находитесь у реки, то есть ваше положение - x = -1.
- * Для перемещения к соседнему растению требуется один шаг.
- *
- * <p>Для расчета количества шагов, необходимых для полива всех растений, реализуйте метод getStepsCount().
- *
- * <p>Входящие параметры метода:
- *
- * <ol>
- * <li>Массив целых чисел, где каждый элемент обозначает объем воды, необходимый соответствующему растению.
- *      Индекс массива - номер растения в саду. Растение не может требовать для полива менее 1 единицы воды;
- * <li>Объем лейки. Обозначен целым числом. Объем лейки не может быть отрицателен или равен нулю.
- * </ol>
- *
- * <p>Возвращаемое значение метода - количество шагов, необходимых для полива всех растений.
- *
- * <p>Пример:
- *
- * <p>Входящие параметры: [2,2,3,3], 5
- *
- * <p>Возвращаемое значение: 14
- *
- * <p>Если сад пуст - количество шагов для его полива равно 0.
- *
- * <p>При наличии некорректных входных данных верните -1.
- *
- * <p>P.S. Решение не должно использовать сортировки, коллекции, Stream API и иной материал, выходящий за рамки
- * пройденного курса.
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
- */
 public class Task13 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        var plants = new int[]{7, 1, 0, 5, 3};
+        var wateringCanVolume = 8;
+
+        System.out.println(getStepsCount(plants, wateringCanVolume));
     }
 
     static int getStepsCount(int[] plants, int wateringCanVolume) {
-        // Ваш код
-        return 0;
+
+        for (int plant : plants) {
+
+            if (plant < 0 || wateringCanVolume < 0 || plant > wateringCanVolume) {
+                return -1;
+            }
+        }
+
+        int currentPosition = -1;
+        int currentWater = wateringCanVolume;
+        int totalSteps = 0;
+        boolean isWatered = false;
+        int notWateredPlants;
+
+        while (!isWatered) {
+
+            notWateredPlants = 0;
+
+            for (int i = 0; i < plants.length; i++) {
+
+                if (plants[i] == 0) {
+                    continue;
+                }
+
+                if (plants[i] > currentWater) {
+                    break;
+                }
+
+                totalSteps += i - currentPosition;
+                currentPosition = i;
+                currentWater -= plants[i];
+                plants[i] = 0;
+            }
+
+            for (int plant : plants) {
+                notWateredPlants += plant;
+            }
+
+            if (notWateredPlants > 0) {
+                totalSteps += currentPosition + 1;
+                currentPosition = -1;
+                currentWater = wateringCanVolume;
+            } else {
+                isWatered = true;
+            }
+
+
+        }
+        return totalSteps;
     }
 }
