5d2b6fed760d77dbafad9ead2fde05be39616a2e
Dmitry <padimych@gmail.com>
2024-12-02 19:04:23 +0300

    task1

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -12,17 +12,22 @@ package com.walking.intensive.chapter1.task1;
  *
  * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
  */
+
 public class Task1 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
         int age = 0;
-
         System.out.println(getAgeString(age));
     }
 
     static String getAgeString(int age) {
-//        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (age < 0) {
+            return "Некорректный ввод";
+        }
+        if (age % 10 == 0 | age % 10 > 4 | age % 100 > 4 & age % 100 < 21) {
+            return "Вам " + age + " лет";
+        } else if (age % 10 == 1) {
+            return "Вам " + age + " год";
+        }
+        return "Вам " + age + " года";
     }
 }
