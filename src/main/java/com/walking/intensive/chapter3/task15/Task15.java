0e28bcc4b3ede61cbcf49bd5e05743a3592582ff
Neverhood <just7500@gmail.com>
2024-12-03 00:11:18 +0800

    Task15

--- src/main/java/com/walking/intensive/chapter3/task15/Task15.java
+++ src/main/java/com/walking/intensive/chapter3/task15/Task15.java
@@ -1,50 +1,64 @@
 package com.walking.intensive.chapter3.task15;
 
-/**
- * Существует город, состоящий из N x N блоков, где каждый блок содержит одно здание в форме вертикальной
- * квадратной призмы. Линия горизонта города — это внешний контур, образованный всеми зданиями,
- * если смотреть на город издалека. Линия горизонта с каждого из сторон света — севера,
- * востока, юга и запада — может отличаться.
- *
- * <p>Каждое здание имеет определенную высоту, измеряемую в этажах.
- *
- * <p>Разрешено увеличивать высоту любого количества зданий на любую величину этажей
- * (величина может быть разной для каждого здания). Высота здания с нулевой высотой также может быть увеличена.
- * Увеличение высоты здания не должно повлиять на горизонт города ни с какой стороны света.
- *
- * <p>Реализуйте метод getMaxFloors() с учетом условий ниже.
- *
- * <p>Входящий параметр: массив city[][], где city[r][c] представляет высоту здания,
- * расположенного в блоке в строке r и столбце c. Высота здания может быть строго положительной.
- *
- * <p>Возвращаемое значение: максимально возможное количество достроенных этажей,
- * на которое можно увеличить высоту зданий без изменения горизонта города
- * с любого направления по сторонам света.
- *
- * <p>Пример:
- *
- * <p>Входящий массив: city[ ][ ] = [[2,1],[1,3]].
- *
- * <p>Возвращаемое значение: 2.
- *
- * <p>Пояснение: всего 4 здания, 2 из которых имеют по 1 этажу, 1 здание - 2 этажа и 1 здание - 3 этажа.
- * Можно добавить максимум по 1 этажу к каждому одноэтажному дому чтобы ни одна из 4 линий горизонта не поменялась.
- * Итого 2 этажа.
- *
- * <p>При наличии некорректных входных данных верните из метода -1.
- *
- * <p>P.S. Решение не должно использовать сортировки, коллекции, Stream API и иной материал, выходящий за рамки
- * пройденного курса.
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
- */
 public class Task15 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+
+        int[][] city = {{3, 0, 8, 4}, {2, 4, 5, 7}, {9, 2, 6, 3}, {0, 3, 1, 0}};
+
+        System.out.println(getMaxFloors(city));
     }
 
     static int getMaxFloors(int[][] city) {
-        // Ваш код
-        return 0;
+
+        if (city.length < 1) {
+            return -1;
+        }
+
+        for (int[] street : city) {
+
+            if (street.length != city.length) {
+                return -1;
+            }
+
+            for (int i = 0; i < city.length; i++) {
+
+                if (street[i] < 0) {
+                    return -1;
+                }
+            }
+        }
+
+        int[] horizontalProjection = new int[city.length];
+        int[] verticalProjection = new int[city.length];
+
+        for (int r = 0; r < city.length; r++) {
+
+            for (int c = 0; c < city[r].length; c++) {
+
+                if (city[r][c] > horizontalProjection[r]) {
+                    horizontalProjection[r] = city[r][c];
+                }
+
+                if (city[r][c] > verticalProjection[c]) {
+                    verticalProjection[c] = city[r][c];
+                }
+            }
+        }
+
+        int maxFloors = 0;
+
+        for (int r = 0; r < city.length; r++) {
+
+            for (int c = 0; c < city.length; c++) {
+
+                if (city[r][c] <= verticalProjection[c])
+                    if (city[r][c] < horizontalProjection[r]) {
+                        city[r][c] += Math.abs(horizontalProjection[r] - verticalProjection[c]);
+                        maxFloors += Math.abs(horizontalProjection[r] - verticalProjection[c]);
+                    }
+            }
+        }
+
+        return maxFloors;
     }
 }
