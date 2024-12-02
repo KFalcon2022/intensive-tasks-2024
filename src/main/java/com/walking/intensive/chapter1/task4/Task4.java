86cf35507ceaa47a85cc58881dbddac093e471d9
artur160990 <artur160990@gmail.com>
2024-12-03 00:29:45 +0300

    task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -34,8 +34,42 @@ public class Task4 {
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
+        if (a == 0 && b == 0 && c == 0) {
+            return "Бесконечное множество решений.";
+        }
 
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && b == 0) {
+            return "Количество решений: 0.";
+        }
+
+        if (a == 0) {
+            double x = -c / b;
+
+            return "Количество решений: 1. Корень: " + x;
+        }
+
+        double discriminant = b * b - 4 * a * c;
+
+        if (discriminant < 0) {
+            return "Количество решений: 0.";
+        }
+
+        double x1, x2;
+
+        if (discriminant == 0) {
+            x1 = -b / (2 * a);
+            return "Количество решений: 1. Корень: " + (x1 + 0.0);
+        }
+
+        x1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+        x2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+        if (x1 > x2) {
+            double buffer = x1;
+            x1 = x2;
+            x2 = buffer;
+        }
+
+        return "Количество решений: 2. Корни: " + (x1 + 0.0) + ";" + (x2 + 0.0);
     }
 }
