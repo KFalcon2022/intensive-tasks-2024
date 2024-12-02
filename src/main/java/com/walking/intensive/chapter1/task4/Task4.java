b984a688afb35f2bc5ae67138107500fc6a59f38
Anton <goptarevanton@gmail.com>
2024-12-02 18:16:17 +0300

    Исправлены замечания

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -30,26 +30,30 @@ public class Task4 {
         double c = 2;
 
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-
-        double x1;
-        double x2;
-
         if (a == 0 && b == 0 && c == 0) {
             return "Бесконечное множество решений.";
         }
 
+        double x1;
+
+        if (a == 0 && b != 0) {
+            x1 = -c / b;
+            return "Количество решений: 1. Корень: " + x1;
+        }
+
         double discriminant = b * b - 4 * a * c;
 
-        if (discriminant < 0 || a == 0 && b == 0) {
+        if (discriminant < 0 || a == 0) {
             return "Количество решений: 0.";
+        }
 
-        } else if (discriminant == 0 || a == 0) {
+        double x2;
 
-            x1 = a == 0 ? (-c / b) : (-b / 2 * a);
+        if (discriminant == 0) {
+            x1 = -b / 2 * a;
             x1 = x1 == -0 ? 0 : x1;
 
             return "Количество решений: 1. Корень: " + x1;
