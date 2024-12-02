1c89cc3df1f0cd1a4b9ea384e4159ab0b66074b3
i.baykalov <ilyabaikalov@gmail.com>
2024-12-02 20:09:07 +0300

    task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -8,7 +8,6 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
     }
 
     /**
@@ -19,8 +18,11 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getLcm(int m, int n) {
-        // Ваш код
-        return 0;
+        if (m <= 0 || n <= 0) {
+            return -1;
+        }
+
+        return m / getGcd(m, n) * n;
     }
 
     /**
@@ -31,8 +33,22 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcd(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (m <= 0 || n <= 0) {
+            return -1;
+        }
+
+        int max = Math.max(m, n);
+        int min = Math.min(m, n);
+        int remainder = min;
+
+        while (remainder != 0) {
+            remainder = max % min;
+            max = min;
+            min = remainder;
+        }
+
+        return max;
     }
 
     /**
@@ -44,7 +60,34 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcdByEuclideanAlgorithm(int m, int n) {
-        // Ваш код
-        return 0;
+
+        if (m <= 0 || n <= 0) {
+            return -1;
+        }
+
+        if (m == n) {
+            return m;
+        }
+
+        if (m == 1 || n == 1) {
+            return 1;
+        }
+
+        if (m % 2 == 0 && n % 2 == 0) {
+            return 2 * getGcd(m / 2, n / 2);
+        }
+
+        if (m % 2 == 0) {
+            return getGcd(m / 2, n);
+        }
+
+        if (n % 2 == 0) {
+            return getGcd(m, n / 2);
+        }
+
+        int max = Math.max(m, n);
+        int min = Math.min(m, n);
+
+        return getGcd((max - min) / 2, min);
     }
 }
