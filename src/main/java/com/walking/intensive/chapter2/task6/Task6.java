d3874f412c58c0a87b73623b903c85be76c50758
Lizasha <violetcactusonline@gmail.com>
2024-12-03 01:11:05 +0100

    task6

--- src/main/java/com/walking/intensive/chapter2/task6/Task6.java
+++ src/main/java/com/walking/intensive/chapter2/task6/Task6.java
@@ -8,7 +8,9 @@ package com.walking.intensive.chapter2.task6;
  */
 public class Task6 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        //System.out.println(getGcd(12, 9));
+        //System.out.println(getLcm(12,9));
+        System.out.println(getGcdByEuclideanAlgorithm(48, 18));
     }
 
     /**
@@ -19,7 +21,17 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getLcm(int m, int n) {
-        // Ваш код
+        if (isInputDataIncorrect(m, n)) {
+            return -1;
+        }
+
+        int maxOfTwoDigits = Math.max(m, n);
+        for (int i = 1; i <= maxOfTwoDigits; i++){
+            int multipleNumber = maxOfTwoDigits * i;
+            if (multipleNumber % m == 0 && multipleNumber % n == 0){
+                return multipleNumber;
+            }
+        }
         return 0;
     }
 
@@ -31,7 +43,17 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcd(int m, int n) {
-        // Ваш код
+        if (isInputDataIncorrect(m, n)) {
+            return -1;
+        }
+
+        int minOfTwoDigits = Math.min(m, n);
+
+        for (int i = minOfTwoDigits; i > 0; i--) {
+            if (m % i == 0 && n % i == 0) {
+                return i;
+            }
+        }
         return 0;
     }
 
@@ -44,7 +66,17 @@ public class Task6 {
      * <p>Если входные данные некорректны - метод должен возвращать -1.
      */
     static int getGcdByEuclideanAlgorithm(int m, int n) {
-        // Ваш код
-        return 0;
+        if (isInputDataIncorrect(m, n)) {
+            return -1;
+        }
+
+        if (n == 0) {
+            return m;
+        }
+            return getGcdByEuclideanAlgorithm(n, m % n);
+    }
+
+    static boolean isInputDataIncorrect(int m, int n) {
+        return m <= 0 || n < 0;
     }
 }
