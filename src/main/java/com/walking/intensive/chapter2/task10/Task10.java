7ee59bc04b842dd04fb31bfe6540475173394e02
i.baykalov <ilyabaikalov@gmail.com>
2024-12-03 16:00:54 +0300

    task10

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -11,11 +11,41 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
+        System.out.println(getStringContainsOnlyLetters("Муза! Ранясь шилом опыта, ты помолишься на разум."));
     }
 
     static boolean isPalindrome(String inputString) {
-        // Ваш код
-        return false;
+        if (inputString == null) {
+            return false;
+        }
+
+        String pureString = getStringContainsOnlyLetters(inputString);
+        int length = pureString.length();
+
+        if (length < 2) {
+            return false;
+        }
+
+        for (int i = 0, j = length - 1; i < j; i++, j--) {
+            if (pureString.charAt(i) != pureString.charAt(j)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    static String getStringContainsOnlyLetters(String s) {
+        String lowerCaseString = s.toLowerCase();
+        String result = lowerCaseString;
+
+        for (int i = 0; i < s.length(); i++) {
+            char currentChar = lowerCaseString.charAt(i);
+
+            if (currentChar < 1072 || currentChar > 1103) {
+                result = result.replace("" + currentChar, "");
+            }
+        }
+
+        return result;
     }
 }
