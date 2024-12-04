a2598a5faad2585ed1bd488741b2541628852249
i.baykalov <ilyabaikalov@gmail.com>
2024-12-04 22:29:35 +0300

    task10 fix

--- src/main/java/com/walking/intensive/chapter2/task10/Task10.java
+++ src/main/java/com/walking/intensive/chapter2/task10/Task10.java
@@ -11,7 +11,7 @@ package com.walking.intensive.chapter2.task10;
  */
 public class Task10 {
     public static void main(String[] args) {
-        System.out.println(getStringContainsOnlyLetters("Муза! Ранясь шилом опыта, ты помолишься на разум."));
+        System.out.println(isPalindrome("Муза! Ранясь шилом опыта, ты помолишься на разум."));
     }
 
     static boolean isPalindrome(String inputString) {
@@ -19,33 +19,26 @@ public class Task10 {
             return false;
         }
 
-        String pureString = getStringContainsOnlyLetters(inputString);
-        int length = pureString.length();
+        String lowerCaseString = inputString.toLowerCase();
+        int length = inputString.length();
 
         if (length < 2) {
             return false;
         }
 
         for (int i = 0, j = length - 1; i < j; i++, j--) {
-            if (pureString.charAt(i) != pureString.charAt(j)) {
+            while (lowerCaseString.charAt(i) < 1072 || lowerCaseString.charAt(i) > 1103) {
+                i++;
+            }
+            while (lowerCaseString.charAt(j) < 1072 || lowerCaseString.charAt(j) > 1103) {
+                j--;
+            }
+
+            if (lowerCaseString.charAt(i) != lowerCaseString.charAt(j)) {
                 return false;
             }
         }
+
         return true;
-    }
-
-    static String getStringContainsOnlyLetters(String s) {
-        String lowerCaseString = s.toLowerCase();
-        String result = lowerCaseString;
-
-        for (int i = 0; i < s.length(); i++) {
-            char currentChar = lowerCaseString.charAt(i);
-
-            if (currentChar < 1072 || currentChar > 1103) {
-                result = result.replace("" + currentChar, "");
-            }
-        }
-
-        return result;
     }
 }
