7b146ab27ed91b64a7ed2af3c19fa06b2ff85c50
rekynkovav <rekynkovav@users.noreply.github.com>
2024-12-04 21:41:56 +0300

    готово

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -14,20 +14,20 @@ package com.walking.intensive.chapter1.task1;
  */
 public class Task1 {
     public static void main(String[] args) {
+        System.out.println(getAgeString(122));
     }
 
     static String getAgeString(int age) {
-
-        if (age <= 0) {
+        if (age <= 0 || age > 127) {
             return "Некорректный ввод";
         }
-        if (age == 1 | (age > 20 && age % 10 == 1)) {
+        if (age == 1 || (age > 20 && age % 10 == 1)) {
             return "Вам " + age + " год";
         }
-        if (age < 5 | (age > 20 & age % 10 == 2 | age % 10 == 3 | age % 10 == 4)) {
+        if (age < 5 || (age > 20 && age < 100 || age > 120 && age % 10 < 5)) {
             return "Вам " + age + " года";
-        } else {
-            return "Вам " + age + " лет";
         }
+        return "Вам " + age + " лет";
     }
 }
+
