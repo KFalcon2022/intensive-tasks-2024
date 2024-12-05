0d29c92e089ff84351b22de02c511e68d045d5d2
xBobrov <andy.beaver.vld@gmail.com>
2024-12-05 11:49:18 +1000

    task11

--- src/main/java/com/walking/intensive/chapter3/task11/Task11.java
+++ src/main/java/com/walking/intensive/chapter3/task11/Task11.java
@@ -42,11 +42,55 @@ package com.walking.intensive.chapter3.task11;
  */
 public class Task11 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
     }
 
     static int getOddSubArraysElementsSum(int[] array) {
-        // Ваш код
-        return 0;
+        if (array.length == 0) {
+            return 0;
+        }
+
+        int oddSubArraysElementSum = 0;
+        int arrayEdge;
+
+        if (array.length >= 5) {
+            arrayEdge = array.length - 2;
+        } else if (array.length == 4) {
+            arrayEdge = array.length - 1;
+        } else {
+            arrayEdge = array.length;
+        }
+
+        for (int i = 0; i < arrayEdge; i++) {
+            int loopSum = getLoopSum(array, i);
+            if (loopSum == -1) {
+                return -1;
+            }
+            oddSubArraysElementSum += loopSum;
+        }
+
+        if (array.length >= 4) {
+            oddSubArraysElementSum += array[array.length - 1];
+        }
+        if (array.length > 4) {
+            oddSubArraysElementSum += array[array.length - 2];
+        }
+        return oddSubArraysElementSum;
+    }
+
+    static int getLoopSum(int[] array, int i) {
+        int loopSum = 0;
+        int tempSum = 0;
+
+        for (int j = i; j < array.length; j++) {
+            if (i == 0 && array[j] <= 0) {
+                return -1;
+            }
+            tempSum += array[j];
+
+            if (i % 2 == 0 && j % 2 == 0 || i % 2 != 0 && j % 2 != 0) {
+                loopSum += tempSum;
+            }
+        }
+        return loopSum;
     }
 }
