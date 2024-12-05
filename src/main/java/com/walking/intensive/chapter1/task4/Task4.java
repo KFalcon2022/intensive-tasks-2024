c7a30967bd651390dbf1f34e2e878f31c16e30cb
vkomarichev <trotill@rambler.ru>
2024-12-05 12:44:43 +0300

    Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -24,18 +24,23 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
         double b = 0;
-        double c = 0;
-
+        double c = -2;
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if ((int)a == 0 && (int)b == 0) {
+            return "Бесконечное множество решений.";
+        }
+        double deskrim = b * b - 4 * a * c;
+        if (deskrim < 0) {
+            return "Количество решений: 0.";
+        }
+        if (deskrim == 0) {
+            return "Количество решений: 1. Корень: " + (int) (-b / 2 * a);
+        }
+        return "Количество решений: 2. Корни: " + (int) ((-b - Math.sqrt(deskrim)) / 2 * a) + "; " + (int) ((-b + Math.sqrt(deskrim)) / 2 * a);
     }
 }
