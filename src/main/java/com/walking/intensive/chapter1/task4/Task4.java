433d27b68346ae009b423a5abd255df0644f42ed
vkomarichev <trotill@rambler.ru>
2024-12-05 16:05:43 +0300

    Task4

--- src/main/java/com/walking/intensive/chapter1/task4/Task4.java
+++ src/main/java/com/walking/intensive/chapter1/task4/Task4.java
@@ -24,18 +24,29 @@ package com.walking.intensive.chapter1.task4;
  */
 public class Task4 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
         double a = 0;
         double b = 0;
-        double c = 0;
-
+        double c = -2.2;
         System.out.println(solveEquation(a, b, c));
-
     }
 
     static String solveEquation(double a, double b, double c) {
-        //        Место для вашего кода
-
-        return null; // Заглушка. При реализации - удалить
+        if (a == 0 && c == 0 && b == 0) {
+            return "Бесконечное множество решений.";
+        }
+        if (a == 0 && c != 0 && b != 0) {
+            return "Количество решений: 1. Корень: " + (-c / b);
+        }
+        double deskrim = b * b - 4 * a * c;
+        if (deskrim < 0 || (a == 0 && b == 0 && c != 0)) {
+            return "Количество решений: 0.";
+        }
+        if (deskrim == 0) {
+            return "Количество решений: 1. Корень: " + (-b / (2 * a));
+        }
+        if (((-b - Math.sqrt(deskrim)) / (2 * a)) > ((-b + Math.sqrt(deskrim)) / (2 * a))){
+            return "Количество решений: 2. Корни: " + ((-b + Math.sqrt(deskrim)) / (2 * a)) + ";" + ((-b - Math.sqrt(deskrim)) / (2 * a));
+        }
+        return "Количество решений: 2. Корни: " + ((-b - Math.sqrt(deskrim)) / (2 * a)) + ";" + ((-b + Math.sqrt(deskrim)) / (2 * a));
     }
 }
