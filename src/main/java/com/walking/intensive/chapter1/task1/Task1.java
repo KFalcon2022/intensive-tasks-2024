7ba2408403355887f83b80122b231955f8fb466f
MarySanovna <marysanovna@gmail.com>
2024-12-07 22:31:52 +0300

    chapter-1 task-2

--- src/main/java/com/walking/intensive/chapter1/task1/Task1.java
+++ src/main/java/com/walking/intensive/chapter1/task1/Task1.java
@@ -15,38 +15,25 @@ package com.walking.intensive.chapter1.task1;
 public class Task1 {
     public static void main(String[] args) {
 //        Для собственных проверок можете делать любые изменения в этом методе
-        int age = 0;
-
+        int age = 213;
         System.out.println(getAgeString(age));
     }
-
     static String getAgeString(int age) {
 
         if (age >= 0) {
-            if ((age % 10 != 2 || age == 112 || age == 12) &&
-                    (age % 10 != 3 || age == 113 || age == 13) &&
-                    (age % 10 != 4 || age == 114 || age == 14)) {
-
+            if ((age % 10 != 2 || age == 112 || age == 12)
+                    && (age % 10 != 3 || age == 113 || age == 13)
+                    && (age % 10 != 4 || age == 114 || age == 14)) {
                 if (age % 10 != 1 || (age == 11 || age == 111)) {
-                    return ("Вам " + age + " лет"); }
-
-                else {
-                    return ("Вам " + age + " год"); }
+                    return ("Вам " + age + " лет");
+                } else {
+                    return ("Вам " + age + " год");
+                }
+            } else {
+                return ("Вам " + age + " года");
             }
-            else {
-                return ("Вам " + age + " года"); }
+        } else {
+            return ("Некорректный ввод");
         }
-        else {
-            return ("Некорректный ввод"); }
     }
 }
-
-
-
-
-
-
-
-
-
-
