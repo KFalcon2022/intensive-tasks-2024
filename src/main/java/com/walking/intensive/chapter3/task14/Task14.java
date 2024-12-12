24b91f614f6fbdbaef6b4163770ffd3be61ebf0b
akrylov <anton.krylov@ice.com>
2024-12-12 13:40:31 +0200

    Task 14

--- src/main/java/com/walking/intensive/chapter3/task14/Task14.java
+++ src/main/java/com/walking/intensive/chapter3/task14/Task14.java
@@ -1,5 +1,7 @@
 package com.walking.intensive.chapter3.task14;
 
+import static java.lang.Math.*;
+
 /**
  * Необходимо разработать программу, которая определяет количество объектов на радарах.
  *
@@ -43,11 +45,56 @@ package com.walking.intensive.chapter3.task14;
  */
 public class Task14 {
     public static void main(String[] args) {
-//        Для собственных проверок можете делать любые изменения в этом методе
     }
 
     static int[] getObjectCounts(int[][] objectLocations, int[][] radars) {
-        // Ваш код
-        return new int[0];
+        if (!isValidInput(objectLocations, radars)) {
+            return new int[0];
+        }
+
+        int[] objectsFound = new int[radars.length];
+
+        for (int i = 0; i < radars.length; i++) {
+            int[] radar = radars[i];
+            int objCounter = 0;
+
+            for (int[] object : objectLocations) {
+                if (isObjectOnRadar(radar, object)) {
+                    objCounter += 1;
+                }
+            }
+
+            objectsFound[i] = objCounter;
+        }
+
+        return objectsFound;
+    }
+
+    static boolean isObjectOnRadar(int[] radar, int[] object) {
+        double d = sqrt(pow(object[0] - radar[0], 2) + pow(object[1] - radar[1], 2));
+
+        return !(d > radar[2]);
+    }
+
+    static boolean isValidInput(int[][] objectLocations, int[][] radars) {
+        for (int[] coordinates : objectLocations) {
+            if (coordinates.length != 2) {
+                return false;
+            }
+            if (coordinates[0] < 0 || coordinates[1] < 0) {
+                return false;
+            }
+        }
+
+        for (int[] radar : radars) {
+            if (radar.length != 3) {
+                return false;
+            }
+            if (radar[2] < 1) {
+                return false;
+            }
+        }
+
+        return true;
     }
 }
