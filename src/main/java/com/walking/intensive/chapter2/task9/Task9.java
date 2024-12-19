7a5e78efbfbb6647abb3420ac1cd75fa7a4c6386
Rita <margarita.mamatkazina@gmail.com>
2024-12-19 15:24:53 +0300

    ready

--- src/main/java/com/walking/intensive/chapter2/task9/Task9.java
+++ src/main/java/com/walking/intensive/chapter2/task9/Task9.java
@@ -1,57 +1,7 @@
 package com.walking.intensive.chapter2.task9;
 
-import java.util.Arrays;
 import java.util.Scanner;
 
-/**
- * Реализуйте метод getPascalTriangle(). Параметром он должен принимать натуральное число N,
- * а метод возвращать строковое представление треугольника Паскаля.
- *
- * <p>Число N — степень или количество этажей в треугольнике.
- *
- * <p>Треугольник должен быть выровнен по центру. Между соседними числами в ряду должен быть ровно 1 пробел.
- *
- * <p>Если входные данные некорректны - метод должен возвращать пустую строку.
- *
- * <p>Примеры:
- *
- * <p>Для N = 5:
- * <pre>
- *     1
- *    1 1
- *   1 2 1
- *  1 3 3 1
- * 1 4 6 4 1
- * </pre>
- *
- * <p>Для N = 18:
- *
- * <pre>
- *                                         1
- *                                        1 1
- *                                       1 2 1
- *                                      1 3 3 1
- *                                     1 4 6 4 1
- *                                   1 5 10 10 5 1
- *                                 1 6 15 20 15 6 1
- *                                1 7 21 35 35 21 7 1
- *                              1 8 28 56 70 56 28 8 1
- *                            1 9 36 84 126 126 84 36 9 1
- *                        1 10 45 120 210 252 210 120 45 10 1
- *                      1 11 55 165 330 462 462 330 165 55 11 1
- *                    1 12 66 220 495 792 924 792 495 220 66 12 1
- *                1 13 78 286 715 1287 1716 1716 1287 715 286 78 13 1
- *            1 14 91 364 1001 2002 3003 3432 3003 2002 1001 364 91 14 1
- *         1 15 105 455 1365 3003 5005 6435 6435 5005 3003 1365 455 105 15 1
- *     1 16 120 560 1820 4368 8008 11440 12870 11440 8008 4368 1820 560 120 16 1
- * 1 17 136 680 2380 6188 12376 19448 24310 24310 19448 12376 6188 2380 680 136 17 1
- * </pre>
- *
- * <p>Обратите внимание - в консоли визуально получается не совсем треугольник - это нормально.
- * Также обращаю внимание на то, что "основание" треугольника не должно иметь лишних отступов.
- *
- * <p><a href="https://github.com/KFalcon2022/intensive-tasks-2024/blob/master/README.md">Требования к оформлению</a>
- */
 public class Task9 {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
@@ -59,26 +9,43 @@ public class Task9 {
         int n = in.nextInt();
         in.close();
 
-        getPascalTriangle(n);
+        System.out.println(getPascalTriangle(n));
     }
 
     static String getPascalTriangle(int n) {
-        int[][] pascalTriangle = new int[n][];
-        pascalTriangle[0] = new int[]{1};
-        pascalTriangle[1] = new int[]{1, 1};
-
-        for (int i = 2; i < pascalTriangle.length; i++) {
+        int[][] triangle = new int[n][];
+        for (int i = 0; i < triangle.length; i++) {
+            triangle[i] = new int[i + 1];
             for (int j = 0; j <= i; j++) {
                 if (j == 0 || j == i) {
-                    pascalTriangle[i][j] = 1;
+                    triangle[i][j] = 1;
                 } else {
-                    pascalTriangle[i][j] = pascalTriangle[i - 1][j] + pascalTriangle[i - 1][j - 1];
+                    triangle[i][j] = triangle[i - 1][j - 1] + triangle[i - 1][j];
                 }
-
             }
-
         }
 
-        return Arrays.toString(pascalTriangle);
+        String lastLine = "";
+        for (int i = 0; i < n; i++) {
+            lastLine += triangle[n - 1][i] + " ";
+        }
+
+        int l = lastLine.length();
+
+        for(int i = 0; i < n - 1; i++) {
+            String pascal = "";
+            for(int j = 0; j <= i; j++) {
+                pascal += triangle[i][j] + " ";
+            }
+            int l2 = pascal.length();
+
+            String line = "";
+            while (line.length() < (l - l2) / 2) {
+                line += " ";
+            }
+            System.out.println(line + pascal);
+        }
+
+        return lastLine;
     }
 }
